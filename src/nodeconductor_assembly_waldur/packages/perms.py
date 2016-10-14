09fa23adfb76f052473ee38de94ce4bdfdcc48e1
Pavel Marchuk <marchukpavelp@gmail.com>
2016-10-14 18:11:38 +0300

    Allow customer owner to create packages
     - wal-26

--- src/nodeconductor_assembly_waldur/packages/perms.py
+++ src/nodeconductor_assembly_waldur/packages/perms.py
@@ -1,8 +1,16 @@
-from nodeconductor.core.permissions import StaffPermissionLogic
-
+from nodeconductor.core.permissions import StaffPermissionLogic, FilteredCollaboratorsPermissionLogic
+from nodeconductor.structure import models as structure_models
 
 PERMISSION_LOGICS = (
     ('packages.PackageTemplate', StaffPermissionLogic(any_permission=True)),
     ('packages.PackageComponent', StaffPermissionLogic(any_permission=True)),
-    ('packages.OpenStackPackage', StaffPermissionLogic(any_permission=True)),
+    ('packages.OpenStackPackage', FilteredCollaboratorsPermissionLogic(
+        collaborators_query=[
+            'tenant__service_project_link__service__customer__roles__permission_group__user',
+        ],
+        collaborators_filter=[
+            {'tenant__service_project_link__service__customer__roles__role_type': structure_models.CustomerRole.OWNER},
+        ],
+        any_permission=True,
+    )),
 )
