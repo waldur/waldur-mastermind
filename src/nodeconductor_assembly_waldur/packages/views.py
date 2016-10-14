965946a4897f6cc52dada042548b581bd72e8639
Pavel Marchuk <marchukpavelp@gmail.com>
2016-10-14 12:23:22 +0300

    Add executor to OpenStackPackage view
     - wal-26

--- src/nodeconductor_assembly_waldur/packages/views.py
+++ src/nodeconductor_assembly_waldur/packages/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, mixins, permissions, filters as rf_filters
 
+from nodeconductor.core import mixins as core_mixins
 from nodeconductor.structure import filters as structure_filters
 
-from . import models, serializers
+from . import models, serializers, executors
 
 
 class PackageTemplateViewSet(viewsets.ReadOnlyModelViewSet):
@@ -12,9 +13,10 @@ class PackageTemplateViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
 
 
-class OpenStackPackageViewSet(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
+class OpenStackPackageViewSet(mixins.CreateModelMixin, core_mixins.CreateExecutorMixin, viewsets.ReadOnlyModelViewSet):
     queryset = models.OpenStackPackage.objects.all()
     serializer_class = serializers.OpenStackPackageSerializer
     lookup_field = 'uuid'
     filter_backends = (structure_filters.GenericRoleFilter, rf_filters.DjangoFilterBackend)
     permission_classes = (permissions.IsAuthenticated, permissions.DjangoObjectPermissions)
+    create_executor = executors.OpenStackPackageCreateExecutor
