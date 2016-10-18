22819c19cbd9bb60b65e0adccd3853d6024b103d
Pavel Marchuk <marchukpavelp@gmail.com>
2016-10-18 17:09:02 +0300

    Fix imports in test_package_templates
     - wal-26

--- src/nodeconductor_assembly_waldur/packages/tests/test_package_templates.py
+++ src/nodeconductor_assembly_waldur/packages/tests/test_package_templates.py
@@ -3,7 +3,7 @@ from rest_framework import test, status
 
 from nodeconductor.structure.tests import factories as structure_factories
 
-from . import PackageTemplateFactory
+from . import factories
 
 
 @ddt
@@ -16,7 +16,7 @@ class PackageTemplateListTest(test.APITransactionTestCase):
     def test_user_can_list_package_templates(self, user):
         self.client.force_authenticate(user=getattr(self, user))
 
-        response = self.client.get(PackageTemplateFactory.get_list_url())
+        response = self.client.get(factories.PackageTemplateFactory.get_list_url())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
@@ -28,8 +28,8 @@ class PackageTemplateRetreiveTest(test.APITransactionTestCase):
 
     @data('staff', 'user')
     def test_user_can_retrieve_package_template(self, user):
-        package_template = PackageTemplateFactory()
+        package_template = factories.PackageTemplateFactory()
         self.client.force_authenticate(user=getattr(self, user))
 
-        response = self.client.get(PackageTemplateFactory.get_url(package_template))
+        response = self.client.get(factories.PackageTemplateFactory.get_url(package_template))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
