8026b5f309264d4e72c3bc503601468cf1cdfcdd
Victor Mireyev <victor@opennodecloud.com>
2016-10-25 23:17:57 +0300

    Enable filtering OpenStack package by customer and project (WAL-49)

--- src/nodeconductor_assembly_waldur/packages/filters.py
+++ src/nodeconductor_assembly_waldur/packages/filters.py
@@ -16,8 +16,9 @@ class PackageTemplateFilter(django_filters.FilterSet):
 
 class OpenStackPackageFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_type='icontains')
-    settings_uuid = UUIDFilter(name='service_settings__uuid')
+    customer = UUIDFilter(name='tenant__service_project_link__project__customer')
+    project = UUIDFilter(name='tenant__service_project_link__project')
 
     class Meta(object):
         model = models.OpenStackPackage
-        fields = ('name', 'settings_uuid',)
+        fields = ('name', 'customer', 'project')
