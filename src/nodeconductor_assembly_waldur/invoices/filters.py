43d8a458011c455dbf8a827cabb7eef4efa5ce0d
Pavel Marchuk <marchukpavelp@gmail.com>
2016-11-08 13:52:48 +0200

    Fix invoice state filter
     - wal-124

--- src/nodeconductor_assembly_waldur/invoices/filters.py
+++ src/nodeconductor_assembly_waldur/invoices/filters.py
@@ -7,6 +7,7 @@ from . import models
 
 class InvoiceFilter(django_filters.FilterSet):
     customer_uuid = UUIDFilter(name='customer__uuid')
+    state = django_filters.MultipleChoiceFilter(choices=models.Invoice.States.CHOICES)
 
     class Meta(object):
         model = models.Invoice
