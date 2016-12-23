19d0a36b3e7d79b2046f39f66b22ee14c59defd1
tarasmatsyk <tamatsyk@gmail.com>
2016-12-23 15:41:40 +0200

    remove redudant url registration

--- src/nodeconductor_assembly_waldur/support/urls.py
+++ src/nodeconductor_assembly_waldur/support/urls.py
@@ -10,7 +10,6 @@ def register_in(router):
     router.register(r'support-issues', views.IssueViewSet, base_name='support-issue')
     router.register(r'support-comments', views.CommentViewSet, base_name='support-comment')
     router.register(r'support-users', views.SupportUserViewSet, base_name='support-user')
-    router.register(r'support-jira-webhook', views.WebHookReceiverViewSet.as_view(), base_name='web_hook_receiver'),
 
 urlpatterns = patterns(
     '',
