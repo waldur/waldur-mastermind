95b076bf2ef4ba50045369dd7e2f977045f7710c
tarasmatsyk <tamatsyk@gmail.com>
2016-12-28 11:34:39 +0200

    move contract to the init file

--- src/nodeconductor_assembly_waldur/support/backend/__init__.py
+++ src/nodeconductor_assembly_waldur/support/backend/__init__.py
@@ -1,1 +1,38 @@
-from atlassian import *
+from django.conf import settings
+
+
+def get_active_backend():
+    return globals()[settings.WALDUR_SUPPORT['ACTIVE_BACKEND']]()
+
+
+class SupportBackendError(Exception):
+    pass
+
+
+class SupportBackend(object):
+    """ Interface for support backend """
+    def create_issue(self, issue):
+        pass
+
+    def update_issue(self, issue):
+        pass
+
+    def delete_issue(self, issue):
+        pass
+
+    def create_comment(self, comment):
+        pass
+
+    def update_comment(self, comment):
+        pass
+
+    def delete_comment(self, comment):
+        pass
+
+    def get_users(self):
+        """
+        This method should return all users that are related to support project on backend.
+
+        Each user should be represented as not saved SupportUser instance.
+        """
+        pass
--- src/nodeconductor_assembly_waldur/support/backend/atlassian.py
+++ src/nodeconductor_assembly_waldur/support/backend/atlassian.py
@@ -8,45 +8,7 @@ from jira import JIRA, JIRAError, Comment
 from jira.utils import json_loads
 
 from nodeconductor_assembly_waldur.support import models
-
-__all__ = ["get_active_backend"]
-
-
-def get_active_backend():
-    return globals()[settings.WALDUR_SUPPORT['ACTIVE_BACKEND']]()
-
-
-class SupportBackendError(Exception):
-    pass
-
-
-class SupportBackend(object):
-    """ Interface for support backend """
-    def create_issue(self, issue):
-        pass
-
-    def update_issue(self, issue):
-        pass
-
-    def delete_issue(self, issue):
-        pass
-
-    def create_comment(self, comment):
-        pass
-
-    def update_comment(self, comment):
-        pass
-
-    def delete_comment(self, comment):
-        pass
-
-    def get_users(self):
-        """
-        This method should return all users that are related to support project on backend.
-
-        Each user should be represented as not saved SupportUser instance.
-        """
-        pass
+from nodeconductor_assembly_waldur.support.backend import SupportBackendError, SupportBackend
 
 
 class JiraBackendError(SupportBackendError):
