7e0e5e1626c2d47698d3d32ad5cec824dccdbd7f
Pavel Marchuk <marchukpavelp@gmail.com>
2017-01-04 16:32:15 +0200

    Fix package creation
     - wal-314
     - use tenant serializer on package creation to avoid duplications

--- src/nodeconductor_assembly_waldur/packages/serializers.py
+++ src/nodeconductor_assembly_waldur/packages/serializers.py
@@ -1,10 +1,9 @@
 from django.db import transaction
-from django.template.defaultfilters import slugify
 from rest_framework import serializers
 
-from nodeconductor.core import utils as core_utils
 from nodeconductor.structure import serializers as structure_serializers, models as structure_models
-from nodeconductor_openstack.openstack import apps as openstack_apps, models as openstack_models
+from nodeconductor_openstack.openstack import (
+    apps as openstack_apps, models as openstack_models, serializers as openstack_serializers)
 from nodeconductor_openstack.openstack_tenant import models as openstack_tenant_models, apps as openstack_tenant_apps
 
 from . import models
@@ -34,52 +33,30 @@ class PackageTemplateSerializer(serializers.HyperlinkedModelSerializer):
         }
 
 
-class OpenStackPackageMixin(structure_serializers.PermissionFieldFilteringMixin):
-    def validate_template(self, template):
-        if template.service_settings.type != openstack_apps.OpenStackConfig.service_name:
-            raise serializers.ValidationError('Template should be related to OpenStack service settings.')
-        elif template.service_settings.state != structure_models.ServiceSettings.States.OK:
-            raise serializers.ValidationError("Template's settings must be in OK state.")
-        return template
-
-    def _set_tenant_quotas(self, tenant, template):
-        components = {c.type: c.amount for c in template.components.all()}
-        for quota_name, component_type in models.OpenStackPackage.get_quota_to_component_mapping().items():
-            tenant.set_quota_limit(quota_name, components[component_type])
+def _check_template_service_settings(serializer, template):
+    """ Template service settings should be in state OK and has type OpenStack """
+    if template.service_settings.type != openstack_apps.OpenStackConfig.service_name:
+        raise serializers.ValidationError('Template should be related to OpenStack service settings.')
+    elif template.service_settings.state != structure_models.ServiceSettings.States.OK:
+        raise serializers.ValidationError("Template's settings must be in OK state.")
+    return template
 
 
-class OpenStackPackageSerializer(OpenStackPackageMixin, serializers.HyperlinkedModelSerializer):
-    name = serializers.CharField(source='tenant.name', help_text='Tenant name.')
-    description = serializers.CharField(
-        required=False, allow_blank=True, source='tenant.description', help_text='Tenant description.')
-    service_project_link = serializers.HyperlinkedRelatedField(
-        source='tenant.service_project_link',
-        view_name='openstack-spl-detail', write_only=True,
-        queryset=openstack_models.OpenStackServiceProjectLink.objects.all())
-    user_username = serializers.CharField(
-        source='tenant.user_username', required=False, allow_null=True,
-        help_text='Tenant user username. By default is generated as <tenant name> + "-user".')
-    user_password = serializers.CharField(
-        source='tenant.user_password', required=False, allow_null=True,
-        help_text='Tenant user password. Leave blank if you want admin password to be auto-generated.')
-    availability_zone = serializers.CharField(
-        source='tenant.availability_zone', required=False, allow_blank=True,
-        help_text='Tenant availability zone.')
+def _set_tenant_quotas(tenant, template):
+    components = {c.type: c.amount for c in template.components.all()}
+    for quota_name, component_type in models.OpenStackPackage.get_quota_to_component_mapping().items():
+        tenant.set_quota_limit(quota_name, components[component_type])
 
-    class Meta(object):
-        model = models.OpenStackPackage
-        fields = ('url', 'uuid', 'name', 'description', 'template', 'service_project_link',
-                  'user_username', 'user_password', 'availability_zone', 'tenant', 'service_settings',)
-        view_name = 'openstack-package-detail'
-        extra_kwargs = {
-            'url': {'lookup_field': 'uuid'},
-            'template': {'lookup_field': 'uuid', 'view_name': 'package-template-detail'},
-            'tenant': {'lookup_field': 'uuid', 'view_name': 'openstack-tenant-detail', 'read_only': True},
-            'service_settings': {'lookup_field': 'uuid', 'read_only': True},
-        }
 
-    def get_filtered_field_names(self):
-        return ('service_settings', 'service_project_link')
+class OpenStackPackageCreateSerializer(openstack_serializers.TenantSerializer):
+    template = serializers.HyperlinkedRelatedField(
+        lookup_field='uuid',
+        view_name='package-template-detail',
+        write_only=True,
+        queryset=models.PackageTemplate.objects.all())
+
+    class Meta(openstack_serializers.TenantSerializer.Meta):
+        fields = openstack_serializers.TenantSerializer.Meta.fields + ('template',)
 
     def validate_service_project_link(self, spl):
         user = self.context['request'].user
@@ -88,9 +65,13 @@ class OpenStackPackageSerializer(OpenStackPackageMixin, serializers.HyperlinkedM
             raise serializers.ValidationError('Only staff, owner or manager can order package.')
         return spl
 
+    validate_template = _check_template_service_settings
+
     def validate(self, attrs):
-        spl = attrs['tenant']['service_project_link']
+        """ Additionally check that template and service project link belong to the same service settings """
         template = attrs['template']
+        attrs = super(OpenStackPackageCreateSerializer, self).validate(attrs)
+        spl = attrs['service_project_link']
         if spl.service.settings != template.service_settings:
             raise serializers.ValidationError(
                 'Template and service project link should be connected to the same service settings.')
@@ -99,21 +80,18 @@ class OpenStackPackageSerializer(OpenStackPackageMixin, serializers.HyperlinkedM
     @transaction.atomic
     def create(self, validated_data):
         """ Create tenant and service settings from it """
-        template = validated_data['template']
-        tenant_data = validated_data['tenant']
-        if not tenant_data.get('availability_zone'):
-            tenant_data['availability_zone'] = template.service_settings.get_option('availability_zone') or ''
-        if not tenant_data.get('user_username'):
-            tenant_data['user_username'] = slugify(tenant_data['name'])[:30] + '-user'
-        if not tenant_data.get('user_password'):
-            tenant_data['user_password'] = core_utils.pwgen()
-        extra_configuration = {'package_name': template.name, 'package_uuid': template.uuid.hex}
-        validated_data['tenant'] = tenant = openstack_models.Tenant.objects.create(
-            extra_configuration=extra_configuration, **tenant_data)
-        self._set_tenant_quotas(tenant, template)
+        template = validated_data.pop('template')
+        tenant = super(OpenStackPackageCreateSerializer, self).create(validated_data)
+        tenant.extra_configuration = {'package_name': template.name, 'package_uuid': template.uuid.hex}
+        tenant.save()
+        _set_tenant_quotas(tenant, template)
         service_settings = self._create_service_settings(tenant)
-        validated_data['service_settings'] = service_settings
-        return super(OpenStackPackageSerializer, self).create(validated_data)
+        package = models.OpenStackPackage.objects.create(
+            tenant=tenant,
+            template=template,
+            service_settings=service_settings,
+        )
+        return package
 
     def _create_service_settings(self, tenant):
         """ Create service settings from tenant and connect them to tenant project. """
@@ -144,7 +122,33 @@ class OpenStackPackageSerializer(OpenStackPackageMixin, serializers.HyperlinkedM
         return service_settings
 
 
-class OpenStackPackageExtendSerializer(OpenStackPackageMixin, serializers.Serializer):
+class OpenStackPackageSerializer(serializers.HyperlinkedModelSerializer):
+    name = serializers.CharField(source='tenant.name', help_text='Tenant name.')
+    description = serializers.CharField(source='tenant.description', help_text='Tenant description.')
+    user_username = serializers.CharField(
+        source='tenant.user_username',
+        help_text='Tenant user username. By default is generated as <tenant name> + "-user".')
+    user_password = serializers.CharField(
+        source='tenant.user_password',
+        help_text='Tenant user password. Leave blank if you want admin password to be auto-generated.')
+    availability_zone = serializers.CharField(
+        source='tenant.availability_zone',
+        help_text='Tenant availability zone.')
+
+    class Meta(object):
+        model = models.OpenStackPackage
+        fields = ('url', 'uuid', 'name', 'description', 'template',
+                  'user_username', 'user_password', 'availability_zone', 'tenant', 'service_settings',)
+        view_name = 'openstack-package-detail'
+        extra_kwargs = {
+            'url': {'lookup_field': 'uuid'},
+            'template': {'lookup_field': 'uuid', 'view_name': 'package-template-detail'},
+            'tenant': {'lookup_field': 'uuid', 'view_name': 'openstack-tenant-detail', 'read_only': True},
+            'service_settings': {'lookup_field': 'uuid', 'read_only': True},
+        }
+
+
+class OpenStackPackageExtendSerializer(structure_serializers.PermissionFieldFilteringMixin, serializers.Serializer):
     package = serializers.HyperlinkedRelatedField(
         view_name='openstack-package-detail',
         lookup_field='uuid',
@@ -158,6 +162,8 @@ class OpenStackPackageExtendSerializer(OpenStackPackageMixin, serializers.Serial
 
     def get_filtered_field_names(self):
         return 'package',
+
+    validate_template = _check_template_service_settings
 
     def validate_package(self, package):
         spl = package.tenant.service_project_link
@@ -197,7 +203,7 @@ class OpenStackPackageExtendSerializer(OpenStackPackageMixin, serializers.Serial
         new_template = validated_data['template']
         service_settings = package.service_settings
         tenant = package.tenant
-        self._set_tenant_quotas(tenant, new_template)
+        _set_tenant_quotas(tenant, new_template)
 
         package.delete()
         new_package = models.OpenStackPackage.objects.create(
