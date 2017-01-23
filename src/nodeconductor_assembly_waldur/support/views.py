9a4d5d14342572f91beeaede5b6a1a7df063a2e6
tarasmatsyk <tamatsyk@gmail.com>
2017-01-23 21:41:02 +0200

    fix configuration caching

--- src/nodeconductor_assembly_waldur/support/views.py
+++ src/nodeconductor_assembly_waldur/support/views.py
@@ -127,11 +127,10 @@ class OfferingViewSet(core_views.ActionsViewSet):
     lookup_field = 'uuid'
     serializer_class = serializers.OfferingSerializer
     unsafe_methods_permissions = [structure_permissions.is_staff]
-    configuration = settings.WALDUR_SUPPORT['OFFERING']
 
     @decorators.list_route()
     def configured(self, request):
-        return response.Response(self.configuration, status=status.HTTP_200_OK)
+        return response.Response(settings.WALDUR_SUPPORT['OFFERING'], status=status.HTTP_200_OK)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
