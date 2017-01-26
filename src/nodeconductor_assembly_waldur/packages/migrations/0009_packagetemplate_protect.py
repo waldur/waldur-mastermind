e03bdb6f88829f1b4565e448b8794a987e930508
tarasmatsyk <tamatsyk@gmail.com>
2017-01-26 17:00:37 +0200

    Add missing migration

---
+++ src/nodeconductor_assembly_waldur/packages/migrations/0009_packagetemplate_protect.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('packages', '0008_package_component_type'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='packagetemplate',
+            name='archived',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AlterField(
+            model_name='openstackpackage',
+            name='template',
+            field=models.ForeignKey(related_name='openstack_packages', on_delete=django.db.models.deletion.PROTECT, to='packages.PackageTemplate', help_text='Tenant will be created based on this template.'),
+        ),
+    ]
