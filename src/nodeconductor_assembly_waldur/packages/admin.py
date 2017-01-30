1fb10df952e9abfda1018ed032c049ddb231126f
tarasmatsyk <tamatsyk@gmail.com>
2017-01-30 11:07:58 +0200

    Allow to set 0 price for a component [WAL-450]

--- src/nodeconductor_assembly_waldur/packages/admin.py
+++ src/nodeconductor_assembly_waldur/packages/admin.py
@@ -50,7 +50,7 @@ class PackageComponentForm(forms.ModelForm):
 
         price_min = 10 ** -models.PackageComponent.PRICE_DECIMAL_PLACES
         monthly_price_min = price_min * 30 * amount
-        if monthly_price < monthly_price_min:
+        if monthly_price < monthly_price_min and monthly_price != 0:
             raise forms.ValidationError('Monthly price for "%s" should be greater than %s' % (type, monthly_price_min))
 
         price_max = 10 ** (models.PackageComponent.PRICE_MAX_DIGITS - models.PackageComponent.PRICE_DECIMAL_PLACES)
