8bad0c8047743c12a8feda1b7f3a48ec33bd4aca
Pavel Marchuk <marchukpavelp@gmail.com>
2017-02-10 10:24:56 +0200

    Improve packages filters
     - wal-490

--- src/nodeconductor_assembly_waldur/invoices/filters.py
+++ src/nodeconductor_assembly_waldur/invoices/filters.py
@@ -1,12 +1,12 @@
 import django_filters
 
-from nodeconductor.core.filters import URLFilter
+from nodeconductor.core import filters as core_filters
 
 from . import models
 
 
 class InvoiceFilter(django_filters.FilterSet):
-    customer = URLFilter(view_name='customer-detail', name='customer__uuid')
+    customer = core_filters.URLFilter(view_name='customer-detail', name='customer__uuid')
     customer_uuid = django_filters.UUIDFilter(name='customer__uuid')
     state = django_filters.MultipleChoiceFilter(choices=models.Invoice.States.CHOICES)
 
@@ -16,7 +16,7 @@ class InvoiceFilter(django_filters.FilterSet):
 
 
 class PaymentDetailsFilter(django_filters.FilterSet):
-    customer = URLFilter(view_name='customer-detail', name='customer__uuid')
+    customer = core_filters.URLFilter(view_name='customer-detail', name='customer__uuid')
     customer_uuid = django_filters.UUIDFilter(name='customer__uuid')
 
     class Meta(object):
