4c01e4af94ca69689897aa381ea2f8217d0cbec8
tarasmatsyk <tamatsyk@gmail.com>
2017-04-11 17:40:20 +0300

    Display username if full_name is not available [WAL-711]

--- src/nodeconductor_assembly_waldur/support/backend/atlassian.py
+++ src/nodeconductor_assembly_waldur/support/backend/atlassian.py
@@ -114,7 +114,7 @@ class JiraBackend(SupportBackend):
         Prepends user info to the comment description to display comment author in JIRA.
         User info format - '[user.full_name user.civil_number]: '.
         """
-        return '[%s %s]: %s' % (comment.author.user.full_name,
+        return '[%s %s]: %s' % (comment.author.user.full_name or comment.author.user.username,
                                 comment.author.user.civil_number or '',
                                 comment.description)
 
