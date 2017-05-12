18c80228fe7b1b2ad202821576d301c324227aaa
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2017-05-12 15:25:58 +0300

    enable support extension for tests
    - WAL-829

--- src/nodeconductor_assembly_waldur/support/tests/test_jira_web_hooks.py
+++ src/nodeconductor_assembly_waldur/support/tests/test_jira_web_hooks.py
@@ -22,6 +22,7 @@ class TestJiraWebHooks(APITestCase):
         self.UPDATED = 'jira:issue_updated'
         self.DELETED = 'jira:issue_deleted'
         jira_backend = 'nodeconductor_assembly_waldur.support.backend.atlassian:JiraBackend'
+        settings.WALDUR_SUPPORT['ENABLED'] = True
         settings.WALDUR_SUPPORT['ACTIVE_BACKEND'] = jira_backend
         jira_request = pkg_resources.resource_stream(__name__, self.JIRA_ISSUE_UPDATE_REQUEST_FILE_NAME).read().decode()
         self.request_data = json.loads(jira_request)
