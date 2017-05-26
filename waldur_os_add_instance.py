ab8e9df5f1dbdd2fe95e32dcc048aa9f0a803a38
tarasmatsyk <tamatsyk@gmail.com>
2017-05-26 18:09:34 +0300

    Change format of waldur module [WAL-760]
    - Reuse access token instead user name and password;
    - Allow to specify an array of hyperlinked resources;
    - Allow to specify uuid or resource name to search for resource;
    - Add a paramater to wait for instance creation;
    - Extract API requests to WaldurClient;
    - Update volume sizes format;
    - Add ssh_key;
    - Allow autoallocation of floating ip;
    - Reuse requests library instead of built-in module utilities;
    - Allow to set single network instead of resources list;

--- waldur_os_add_instance.py
+++ waldur_os_add_instance.py
@@ -1,192 +1,275 @@
 #!/usr/bin/python
 
 DOCUMENTATION = '''
----
-module: waldur_os_add_instance
-short_description: Create OpenStack instance
-'''
+    ---
+    module: waldur_os_add_instance
+    short_description: Create OpenStack instance
+    '''
 
 EXAMPLES = '''
+# TODO [TM:5/26/17] update examples
 - hosts: localhost
   tasks:
-    - name: Create OpenStack instance
-      waldur_os_add_instance:
-        name: 'OS Instance'
-        service_uuid: 'ca8217c97bf74089b1e1131c69f8ad40'
-        project_uuid: '77c23dbcb94a4ad5bba54d82c17fa862'
-        subnet: 'vpc-1-tm-sub-net'
-        flavor: 'm1.micro'
-        image: 'TestVM'
-        system_volume_size: 1024
-        url: 'http://localhost:8000'
-        url_username: 'username'
-        url_password: 'userpassword'
+    - name: Test that my module works
+      waldur_os_add_instance: 
+        name: test instance 2
+        provider: VPC #1 [TM]
+        project: OpenStack Project
+        networks:
+          -
+            subnet: vpc-1-tm-sub-net
+            floating_ip: auto
+        flavor: m1.micro
+        image: TestVM
+        system_volume_size: 1
+        ssh_key: macOs.pub
+        waldur_url: http://localhost:8000
+        access_token: b83557fd8e2066e98f27dee8f3b3433cdc4183ce
       register: result
 
-- hosts: localhost
-  tasks:
-    - name: Create OpenStack instance with data_volume
-      waldur_os_add_instance:
-        name: 'OS Instance'
-        service_uuid: 'ca8217c97bf74089b1e1131c69f8ad40'
-        project_uuid: '77c23dbcb94a4ad5bba54d82c17fa862'
-        subnet: 'vpc-1-tm-sub-net'
-        flavor: 'm1.micro'
-        image: 'TestVM'
-        system_volume_size: 1024
-        data_volume_size: 1024
-        url: 'http://localhost:8000'
-        url_username: 'username'
-        url_password: 'userpassword'
-      register: result
+    - debug: var=result
+    '''
 
-- hosts: localhost
-  tasks:
-    - name: Create OpenStack instance with user_data
-      waldur_os_add_instance:
-        name: 'OS Instance'
-        service_uuid: 'ca8217c97bf74089b1e1131c69f8ad40'
-        project_uuid: '77c23dbcb94a4ad5bba54d82c17fa862'
-        subnet: 'vpc-1-tm-sub-net'
-        flavor: 'm1.micro'
-        image: 'TestVM'
-        system_volume_size: 1024
-        url: 'http://localhost:8000'
-        url_username: 'username'
-        url_password: 'userpassword'
-        user_data: 'This is my user data'
-      register: result
-'''
-
+# extract client to a separate file
+from collections import namedtuple
+from uuid import UUID
 import json
-
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.urls import fetch_url
-
-HEADERS = {'Content-Type': 'application/json'}
+import requests
+import time
 
 
-def response_to_json(response):
-    return json.loads(response.read())
+class WaldurClientException(Exception):
+    pass
 
 
-def get_auth_token(url, module):
-    payload = {
-        'username': module.params.get('url_username'),
-        'password': module.params.get('url_password'),
-    }
-    url = '%s/%s' % (url, 'api-auth/password/')
-    response, info = fetch_url(module, url=url, headers=HEADERS, data=json.dumps(payload), method='POST')
-    if info['status'] >= 400:
-        module.fail_json(msg=info['body'])
-    return response_to_json(response)['token']
+class WaldurClient(object):
+    resource_stable_states = ['OK', 'ERRED']
 
+    class Endpoints(object):
+        Provider = 'openstacktenant'
+        Project = 'projects'
+        Flavor = 'openstacktenant-flavors'
+        Subnet = 'openstacktenant-subnets'
+        FloatingIP = 'openstacktenant-floating-ips'
+        Image = 'openstacktenant-images'
+        ServiceProjectLink = 'openstacktenant-service-project-link'
+        Instance = 'openstacktenant-instances'
+        SshKey = 'keys'
 
-def get_service_project_link(host, token, module):
-    service = module.params.get('service_uuid')
-    project = module.params.get('project_uuid')
-    params = 'service_uuid=%s&project_uuid=%s' % (service, project)
-    url = '%s/%s?%s' % (host, 'api/openstacktenant-service-project-link/', params)
-    response, info = fetch_url(module, url=url, headers=HEADERS, method='GET')
-    if info['status'] >= 400:
-        module.fail_json(msg=info['body'])
-    return response_to_json(response)
+    def __init__(self, host, token):
+        self.host = host
+        self.headers = {
+            'Authorization': 'token %s' % token,
+            'Content-Type': 'application/json',
+        }
 
+    def _build_url(self, endpoint):
+        return requests.compat.urljoin(self.host, 'api/%s/' % endpoint)
 
-def get_flavor(host, token, module):
-    url = '%s/%s?name=%s' % (host, 'api/openstacktenant-flavors/', module.params.get('flavor'))
-    response, info = fetch_url(module, url=url, headers=HEADERS, method='GET')
-    if info['status'] >= 400:
-        module.fail_json(msg=info['body'])
-    return response_to_json(response)
+    def _raise_request_failed(self, response):
+        try:
+            reason = response.json()
+        except ValueError as error:
+            reason = error.message
+        details = 'Status: %s. Reason: %s.' % (response.status_code, reason)
+        raise WaldurClientException('Server refuses to communicate. %s' % details)
 
-def get_image(host, token, module):
-    url = '%s/%s?name=%s' % (host, 'api/openstacktenant-images/', module.params.get('image'))
-    response, info = fetch_url(module, url=url, headers=HEADERS, method='GET')
-    if info['status'] >= 400:
-        module.fail_json(msg=info['body'])
+    def _query_resource(self, endpoint, query_params):
+        url = self._build_url(endpoint)
+        try:
+            response = requests.get(url, params=query_params, headers=self.headers)
+        except requests.exceptions.RequestException as error:
+            raise WaldurClientException(error.message)
 
-    return response_to_json(response)
+        if response.status_code >= 400:
+            self._raise_request_failed(response)
 
-def get_subnet(host, token, module):
-    url = '%s/%s?name=%s' % (host, 'api/openstacktenant-subnets/', module.params.get('subnet'))
-    response, info = fetch_url(module, url=url, headers=HEADERS, method='GET')
-    if info['status'] >= 400:
-        module.fail_json(msg=info['body'])
+        result = response.json()
+        if len(result) > 1:
+            message = 'Ambiguous reference to resource. Query: %s' % query_params
+            raise WaldurClientException(message)
 
-    return response_to_json(response)
+        return result[0] if result else None
 
-def create_instance(host, token, module):
-    spls = get_service_project_link(host, token, module)
-    if len(spls) > 1:
-        module.fail_json(msg='ambigious reference to the service project link')
+    def _get_resource(self, endpoint, value):
+        try:
+            uuid = UUID(value)
+        except ValueError:
+            resource = self._query_resource_by_name(endpoint, value)
+        else:
+            resource = self._query_resource_by_uuid(endpoint, uuid)
 
-    flavors = get_flavor(host, token, module)
-    if len(flavors) > 1:
-        module.fail_json(msg='ambigious reference to the flavor')
+        return resource
 
-    images = get_image(host, token, module)
-    if len(images) > 1:
-        module.fail_json(msg='ambigious reference to the image')
+    def _query_resource_by_uuid(self, endpoint, value):
+        return self._query_resource(endpoint, {'uuid': value})
 
-    subnets = get_subnet(host, token, module)
-    if len(subnets) > 1:
-        module.fail_json(msg='ambigious reference to the subnet')
+    def _query_resource_by_name(self, endpoint, value):
+        return self._query_resource(endpoint, {'name': value})
 
-    service_project_link = spls[0]
-    flavor = flavors[0]
-    image = images[0]
-    subnet = subnets[0]
+    def _get_service_project_link(self, provider_uuid, project_uuid):
+        query = {'project_uuid': project_uuid, 'service_uuid': provider_uuid}
+        return self._query_resource(self.Endpoints.ServiceProjectLink, query)
 
-    url = '%s/%s' % (host, 'api/openstacktenant-instances/')
-    payload = {
-        'name': module.params.get('name'),
-        'flavor': flavor['url'],
-        'image': image['url'],
-        'service_project_link' : service_project_link['url'],
-        'system_volume_size': module.params.get('system_volume_size'),
-        'internal_ips_set':[
-            {'subnet': subnet['url']},
-        ]
-    }
+    def _create_resource(self, endpoint, payload=None):
+        url = self._build_url(endpoint)
+        try:
+            response = requests.post(url, data=json.dumps(payload), headers=self.headers)
+        except requests.exceptions.RequestException as error:
+            raise WaldurClientException(error.message)
 
-    data_volume_size = module.params.get('data_volume_size')
-    if isinstance(data_volume_size, basestring):
-        payload.update({'data_volume_size':data_volume_size})
-    user_data = module.params.get('user_data')
-    if isinstance(user_data, basestring):
-        payload.update({'user_data':user_data})
+        if response.status_code != 201:
+            self._raise_request_failed(response)
 
-    response, info = fetch_url(module, url=url, headers=HEADERS, data=json.dumps(payload), method='POST')
-    if info['status'] >= 400:
-        module.fail_json(msg=info['body'])
+        return response.json()
 
-    has_changed = info['status'] == 201
-    return (has_changed, response_to_json(response)['url'])
+    def _allocate_floating_ip(self, provider_uuid):
+        endpoint = requests.compat.urljoin(self.Endpoints.Provider, provider_uuid)
+        return _create_resource(endpoint)
+
+    def _networks_to_payload(self, networks, provider_uuid):
+        subnets = []
+        floating_ips = []
+
+        for item in networks:
+            subnet_resource = self._get_resource(self.Endpoints.Subnet, item['subnet'])
+            subnet = {'subnet': subnet_resource['url']}
+            subnets.append(subnet)
+            ip_name = item.get('floating_ip')
+            if ip_name:
+                ip = subnet.copy()
+                if ip_name != 'auto':
+                    floating_ip_resource = self._get_resource(self.Endpoints.FloatingIP, ip_name)
+                    ip.update({'url': floating_ip_resource['url']})
+                floating_ips.append(ip)
+
+        return subnets, floating_ips
+
+    def create_instance(
+            self,
+            name,
+            provider,
+            project,
+            networks,
+            flavor,
+            image,
+            system_volume_size,
+            interval=10,
+            timeout=60,
+            wait=None,
+            ssh_key=None,
+            data_volume_size=None,
+            user_data=None):
+        '''
+        TODO: add documentation.
+        '''
+        provider = self._get_resource(self.Endpoints.Provider, provider)
+        project = self._get_resource(self.Endpoints.Project, project)
+        service_project_link = self._get_service_project_link(
+            provider_uuid=provider['uuid'],
+            project_uuid=project['uuid'])
+        flavor = self._get_resource(self.Endpoints.Flavor, flavor)
+        image = self._get_resource(self.Endpoints.Image, image)
+        subnets, floating_ips = self._networks_to_payload(networks, provider['uuid'])
+
+        payload = {
+            'name': name,
+            'flavor': flavor['url'],
+            'image': image['url'],
+            'service_project_link': service_project_link['url'],
+            'system_volume_size': system_volume_size * 1024,
+            'internal_ips_set': subnets,
+            'floating_ips': floating_ips,
+        }
+
+        # TODO: security groups.
+        if data_volume_size:
+            payload.update({'data_volume_size': data_volume_size * 1024})
+        if user_data:
+            payload.update({'user_data': user_data})
+        if ssh_key:
+            ssh_key = self._get_resource(self.Endpoints.SshKey, ssh_key)
+            payload.update({'ssh_public_key': ssh_key['url']})
+
+        instance = self._create_resource(self.Endpoints.Instance, payload)
+
+        if wait:
+            ready = self._is_resource_ready(self.Endpoints.Instance, instance['uuid'])
+            waited = 0
+            while not ready:
+                time.sleep(interval)
+                ready = self._is_resource_ready(self.Endpoints.Instance, instance['uuid'])
+                waited += interval
+                if waited >= interval:
+                    error = 'Instance "%s" has not changed state to stable' % instance['url']
+                    message = '%s. Seconds passed: %s' % (error, timeout)
+                    raise WaldurClientException(message)
+
+        return instance
+
+    def _is_resource_ready(self, endpoint, uuid):
+        resource = self._query_resource_by_uuid(endpoint, uuid)
+        return resource in self.resource_stable_states
+# end client
 
 
 def main():
     fields = {
+        'waldur_url': {'required': True, 'type': 'str'},
+        'access_token': {'required': True, 'type': 'str'},
         'name': {'required': True, 'type': 'str'},
-        'service_uuid': {'required': True, 'type': 'str'},
-        'project_uuid': {'required': True, 'type': 'str'},
-        'subnet': {'required': True, 'type': 'str'},
+        'provider': {'required': True, 'type': 'str'},
+        'project': {'required': True, 'type': 'str'},
+        'networks': {'required': False, 'type': 'list'},
+        'subnet': {'required': False, 'type': 'str'},
+        'floating_ip': {'required': False, 'type': 'str'},
         'flavor': {'required': True, 'type': 'str'},
         'image': {'required': True, 'type': 'str'},
         'system_volume_size': {'required': True, 'type': 'int'},
-        'data_volume_size': {'required': False, 'type': 'int'},
-        'user_data': {'required': False, 'type': 'str'},
-        'url': {'required': True, 'type': 'str'},
-        'url_username': {'required': True, 'type': 'str'},
-        'url_password': {'required': True, 'type': 'str'},
+        'data_volume_size': {'type': 'int'},
+        'ssh_key': {'type': 'str'},
+        'user_data': {'type': 'str'},
+        'wait': {'default': True, 'type': 'bool'},
+        'timeout': {'default': 60, 'type': 'int'},
+        'interval': {'default': 10, 'type': 'int'}
     }
-    module = AnsibleModule(argument_spec=fields)
-    url = module.params.get('url')
-    token = get_auth_token(url, module)
-    HEADERS.update({'Authorization': 'token %s' % token})
-    has_changed, meta = create_instance(url, token, module)
-    module.exit_json(changed=has_changed, meta=meta)
+    required_together = [['wait','timeout'], ['subnet', 'floating_ip']]
+    mutually_exclusive = [['subnet', 'networks'], ['floating_ip', 'networks']]
+    required_one_of = [['subnet', 'networks'], ['floating_ip','networks']]
+    module = AnsibleModule(
+        argument_spec=fields,
+        required_together=required_together,
+        required_one_of=required_one_of,
+        mutually_exclusive=mutually_exclusive)
+
+    client = WaldurClient(module.params['waldur_url'], module.params['access_token'])
+    networks = module.params.get('networks', {
+        'subnet': module.params['subnet'],
+        'floating_ip': module.params['floating_ip']
+        })
+    try:
+        instance = client.create_instance(
+            name=module.params['name'],
+            provider=module.params['provider'],
+            project=module.params['project'],
+            networks=networks,
+            flavor=module.params['flavor'],
+            image=module.params['image'],
+            system_volume_size=module.params['system_volume_size'],
+            data_volume_size=module.params['data_volume_size'],
+            ssh_key=module.params['ssh_key'],
+            wait=module.params['wait'],
+            interval=module.params['interval'],
+            timeout=module.params['timeout'],
+            user_data=module.params['user_data'])
+    except WaldurClientException as error:
+        module.fail_json(msg=error.message)
+
+    module.exit_json(meta=instance)
 
 
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.urls import fetch_url
+from ansible.module_utils.waldur_client import WaldurClient, WaldurClientException
 if __name__ == '__main__':
     main()
