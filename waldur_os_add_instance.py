7b9e9ff7feb223f80ba219bb000ce521da7e297c
tarasmatsyk <tamatsyk@gmail.com>
2017-05-29 17:54:56 +0300

    Update documentation [WAL-760]

--- waldur_os_add_instance.py
+++ waldur_os_add_instance.py
@@ -9,135 +9,134 @@ short_description: "Create OpenStack instance"
 version_added: "0.1"
 description: 
   - "Create an OpenStack instance"
+requirements:
+  - "python = 2.7"
+  - "requests"
 options: 
   access_token: 
     description: 
-      - "An access token which has permissions to create an OpenStack instances."
+      - An access token which has permissions to create an OpenStack instances.
     required: true
   api_url: 
     description: 
-      - "Fully qualified url to the Waldur."
+      - Fully qualified url to the Waldur.
     required: true
   data_volume_size: 
-    default: "volume is not created."
+    default: volume is not created.
     description: 
-      - "The size of the data volume in GB."
+      - The size of the data volume in GB.
     required: false
   flavor: 
     description: 
-      - "The name or id of the flavor to use."
+      - The name or id of the flavor to use.
     required: true
   floating_ip: 
     description: 
-      - "An id or address of the existing floating IP to use. 
-      Not assigned if not specified. Use `auto` to allocate new floating IP or reuse available one."
+      - An id or address of the existing floating IP to use. 
+      Not assigned if not specified. Use `auto` to allocate new floating IP or reuse available one.
     required: 
-      - "If a `networks` parameter is not provided."
+      - If a `networks` parameter is not provided.
   image: 
     description: 
-      - "The name or id of the image to use."
+      - The name or id of the image to use.
     required: true
   interval: 
     default: 20
     description: 
-      - "An interval of the instance state polling."
+      - An interval of the instance state polling.
     required: false
   name: 
     description: 
-      - "The name of the new OpenStack instance."
+      - The name of the new OpenStack instance.
     required: true
   networks: 
     description: 
-      - "A list of networks an instance has to be attached to."
+      - A list of networks an instance has to be attached to.
     floating_ip: 
       description: 
-        - "An id or address of the existing floating IP to use. Not assigned if not specified. 
-        Use `auto` to allocate new floating IP or reuse available one."
+        - An id or address of the existing floating IP to use. Not assigned if not specified. 
+        Use `auto` to allocate new floating IP or reuse available one.
       required: true
-    required: "Only if `subnet` and `floating_ip` are not provided"
+    required: Only if `subnet` and `floating_ip` are not provided
     subnet: 
       description: 
-        - "The name or id of the subnet to use."
+        - The name or id of the subnet to use.
       required: true
   project: 
     description: 
-      - "The name or id of the project to add an instance to."
+      - The name or id of the project to add an instance to.
     required: true
   provider: 
     description: 
-      - "The name or id of the instance provider."
+      - The name or id of the instance provider.
     required: true
   security_groups: 
     default: default
     description: 
-      - "A list of ids or names of security groups to apply to the newly created instance."
+      - A list of ids or names of security groups to apply to the newly created instance.
     required: false
   ssh_key: 
     description: 
-      - "The name or id of the SSH key to attach to the newly created instance."
+      - The name or id of the SSH key to attach to the newly created instance.
     required: false
   subnet: 
     description: 
-      - "The name or id of the subnet to use."
+      - The name or id of the subnet to use.
     required: 
-      - "If a `networks` parameter is not provided."
+      - If a `networks` parameter is not provided.
   system_volume_size: 
     description: 
-      - "The size of the system volume in GBs."
+      - The size of the system volume in GBs.
     required: true
   timeout: 
     default: "60 * 10"
     description: 
-      - "The maximum amount of seconds to wait until the instance provisioning is finished."
+      - The maximum amount of seconds to wait until the instance provisioning is finished.
     required: false
   user_data: 
     description: 
-      - "An additional data that will be added to the instance on provisioning."
+      - An additional data that will be added to the instance on provisioning.
     required: false
   wait: 
     default: true
     description: 
-      - "A boolean value that defines whether client has to wait until the instance 
-      provisioning is finished."
+      - A boolean value that defines whether client has to wait until the instance 
+      provisioning is finished.
     required: false
-
-    requirements:
-        - "python = 2.7"
-        - "requests"
     '''
 
 EXAMPLES = '''
 - 
-  name: "Provision a warehouse instance"
+  name: Provision a warehouse instance
   waldur_os_add_instance: 
     access_token: b83557fd8e2066e98f27dee8f3b3433cdc4183ce
-    api_url: "https://waldur.example.com:8000"
+    api_url: https://waldur.example.com:8000
     data_volume_size: 100
     flavor: m1.micro
-    image: "Ubuntu 14.04"
-    name: "Warehouse instance"
+    image: Ubuntu 14.04
+    name: Warehouse instance
     networks: 
       - 
         floating_ip: auto
         subnet: vpc-1-tm-sub-net
       - 
-        floating_ip: "192.101.13.124"
+        floating_ip: 192.101.13.124
         subnet: vpc-1-tm-sub-net-2
-    project: "OpenStack Project"
+    project: OpenStack Project
     provider: VPC
     security_groups: 
       - web
         
 - 
-  name: "Provision build instance"
+  name: Provision build instance
   waldur_os_add_instance: 
     access_token: b83557fd8e2066e98f27dee8f3b3433cdc4183ce
-    api_url: "https://waldur.example.com:8000"
+    api_url: https://waldur.example.com:8000
     flavor: m1.micro
     floating_ip: auto
-    image: "CentOS 7"
-    name: "Build instance"
-    project: "OpenStack Project"
+    image: CentOS 7
+    name: Build instance
+    project: OpenStack Project
     provider: VPC
     ssh_key: ssh1.pub
     subnet: vpc-1-tm-sub-net-2
@@ -150,41 +149,20 @@ EXAMPLES = '''
           expire: False
         
 - 
-  name: "Trigger master instance"
+  name: Trigger master instance
   waldur_os_add_instance: 
     access_token: b83557fd8e2066e98f27dee8f3b3433cdc4183ce
-    api_url: "https://waldur.example.com:8000"
+    api_url: https://waldur.example.com:8000
     flavor: m1.micro
     floating_ip: auto
-    image: "CentOS 7"
-    name: "Build instance"
-    project: "OpenStack Project"
+    image: CentOS 7
+    name: Build instance
+    project: OpenStack Project
     provider: VPC
     ssh_key: ssh1.pub
     subnet: vpc-1-tm-sub-net-2
     system_volume_size: 40
     wait: false
-    
-
-Variables as parameters
-- 
-  name: "Trigger master instance"
-  waldur_os_add_instance: 
-    access_token: "{{ access_token }}"
-    api_url: "{{ api_url }}"
-    flavor: m1.micro
-    floating_ip: auto
-    image: "CentOS 7"
-    name: "{{ instance_name }}"
-    project: "OpenStack Project"
-    provider: VPC
-    ssh_key: ssh1.pub
-    subnet: vpc-1-tm-sub-net-2
-    system_volume_size: 40
-    wait: false
-    
-To pass parameters through the command line:
-ansible-playbook my_playbook.yml -e "access_token=token name=Instance Name api_url=https://waldur.example.com:8000"
 
     '''
 
