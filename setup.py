4521221b7ecaffe0ca0475df7520671b7b5286d2
Jenkins <jenkins@opennodecloud.com>
2017-07-14 14:17:58 +0000

    Preparing new release: 2.6.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requires = [
 
 setup(
     name='nodeconductor-assembly-waldur',
-    version='2.6.3',
+    version='2.6.4',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
