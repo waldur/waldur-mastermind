25a8917d577120c0bb5c4adffa342b0af69338ff
tarasmatsyk <tamatsyk@gmail.com>
2017-08-29 15:35:47 +0300

    Move issue creation logger under JiraBackend [WAL-1077]
    Backend issue key is missing as handler is fired right after waldur
    issue is saved and before JIRA issue is populated.

--- src/nodeconductor_assembly_waldur/support/handlers.py
+++ src/nodeconductor_assembly_waldur/support/handlers.py
@@ -6,19 +6,14 @@ from . import tasks, models
 
 def log_issue_save(sender, instance, created=False, **kwargs):
     if created:
-        event_logger.waldur_issue.info(
-            'Issue {issue_key} has been created.',
-            event_type='issue_creation_succeeded',
-            event_context={
-                'issue': instance,
-            })
-    else:
-        event_logger.waldur_issue.info(
-            'Issue {issue_key} has been updated.',
-            event_type='issue_update_succeeded',
-            event_context={
-                'issue': instance,
-            })
+        return
+
+    event_logger.waldur_issue.info(
+        'Issue {issue_key} has been updated.',
+        event_type='issue_update_succeeded',
+        event_context={
+            'issue': instance,
+        })
 
 
 def log_issue_delete(sender, instance, **kwargs):
