9cfd1aa57abc793b85d31dbfab8f0e60b9252407
Victor Mireyev <victor@opennodecloud.com>
2017-09-22 13:02:47 +0300

    Render SLURM package name [WAL-1163]

--- src/nodeconductor_assembly_waldur/slurm_invoices/serializers.py
+++ src/nodeconductor_assembly_waldur/slurm_invoices/serializers.py
@@ -6,7 +6,7 @@ from . import models
 class SlurmPackageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta(object):
         model = models.SlurmPackage
-        fields = ('uuid', 'url', 'service_settings', 'cpu_price', 'gpu_price', 'ram_price')
+        fields = ('uuid', 'url', 'service_settings',  'name', 'cpu_price', 'gpu_price', 'ram_price')
         extra_kwargs = {
             'url': {'lookup_field': 'uuid', 'view_name': 'slurm-package-detail'},
             'service_settings': {'lookup_field': 'uuid', 'read_only': True},
