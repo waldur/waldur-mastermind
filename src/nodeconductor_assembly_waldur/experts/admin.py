dca4ed177887e926005ed6d87407201f04f7ad4d
Victor Mireyev <victor@opennodecloud.com>
2017-10-18 12:03:21 +0300

    Render extra parameters of expert request using JSON editor.

--- src/nodeconductor_assembly_waldur/experts/admin.py
+++ src/nodeconductor_assembly_waldur/experts/admin.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.forms import ModelForm
+from jsoneditor.forms import JSONEditor
 
 from . import models
 
@@ -9,9 +11,17 @@ class ExpertProviderAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'customer', 'created')
 
 
+class ExpertRequestModel(ModelForm):
+    class Meta:
+        widgets = {
+            'extra': JSONEditor(),
+        }
+
+
 class ExpertRequestAdmin(admin.ModelAdmin):
     list_display = ('uuid', 'project', 'state', 'created')
     readonly_fields = ('project', 'created')
+    form = ExpertRequestModel
 
 
 admin.site.register(models.ExpertProvider, ExpertProviderAdmin)
