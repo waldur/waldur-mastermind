da8fd94eb7e387fc37c63f7a392f6e2b414a54cf
Victor Mireyev <victor@opennodecloud.com>
2017-11-07 20:00:01 +0300

    SAF export email should put all organizations into one email [WAL-1207]

--- src/nodeconductor_assembly_waldur/invoices/tests/test_report.py
+++ src/nodeconductor_assembly_waldur/invoices/tests/test_report.py
@@ -1,19 +1,25 @@
-from django.test import TestCase
+import datetime
+import mock
+
+from django.test import TransactionTestCase
+from django.utils import timezone
 from freezegun import freeze_time
 
 from nodeconductor_assembly_waldur.invoices.tasks import format_invoice_csv
 
-from .. import models
+from .. import models, tasks
 from . import fixtures, factories, utils
 
 
-class BaseReportFormatterTest(TestCase):
+class BaseReportFormatterTest(TransactionTestCase):
     def setUp(self):
         fixture = fixtures.InvoiceFixture()
         package = fixtures.create_package(100, fixture.openstack_tenant)
         package.template.name = 'PackageTemplate'
         package.template.save()
-        invoice = models.Invoice.objects.get(customer=package.tenant.service_project_link.project.customer)
+        self.customer = package.tenant.service_project_link.project.customer
+
+        invoice = models.Invoice.objects.get(customer=self.customer)
         invoice.customer.agreement_number = 100
         invoice.customer.save()
         invoice.set_created()
@@ -44,7 +50,7 @@ class GenericReportFormatterTest(BaseReportFormatterTest):
         self.assertTrue('OFFERING-001' in lines[-1])
 
 
-@utils.override_invoices_settings(INVOICE_REPORTING={
+INVOICE_REPORTING = {
     'ENABLE': True,
     'USE_SAF': True,
     'SAF_PARAMS': {
@@ -54,7 +60,11 @@ class GenericReportFormatterTest(BaseReportFormatterTest):
     'CSV_PARAMS': {
         'delimiter': str(';'),
     },
-})
+    'EMAIL': 'test@example.com',
+}
+
+
+@utils.override_invoices_settings(INVOICE_REPORTING=INVOICE_REPORTING)
 @freeze_time('2017-09-26')
 class SafReportFormatterTest(BaseReportFormatterTest):
     def test_invoice_items_are_properly_formatted(self):
@@ -70,3 +80,83 @@ class SafReportFormatterTest(BaseReportFormatterTest):
         expected_data = '30.09.2017;26.09.2017;26.10.2017;100;100;;5;1500.00;0.00;20%;' \
                         'PROJEKT; (Small / PackageTemplate);;;01.09.2017-30.09.2017'
         self.assertEqual(lines[1], expected_data)
+
+
+@freeze_time('2017-11-01')
+@mock.patch('nodeconductor_assembly_waldur.invoices.tasks.send_mail')
+class InvoiceReportTaskTest(BaseReportFormatterTest):
+    @utils.override_invoices_settings(
+        INVOICE_REPORTING=INVOICE_REPORTING,
+        ENABLE_ACCOUNTING_START_DATE=True,
+    )
+    def test_demo_customers_are_skipped_if_accounting_start_is_enabled(self, send_mail_mock):
+        factories.PaymentDetailsFactory(
+            customer=self.customer,
+            accounting_start_date=timezone.now() + datetime.timedelta(days=10)
+        )
+        tasks.send_invoice_report()
+        message = send_mail_mock.call_args[0][1]
+        lines = message.splitlines()
+        self.assertEqual(0, len(lines))
+
+    @utils.override_invoices_settings(
+        INVOICE_REPORTING=INVOICE_REPORTING,
+        ENABLE_ACCOUNTING_START_DATE=False,
+    )
+    def test_demo_customers_are_not_skipped_if_accounting_start_is_not_enabled(self, send_mail_mock):
+        factories.PaymentDetailsFactory(
+            customer=self.customer,
+            accounting_start_date=timezone.now() + datetime.timedelta(days=10)
+        )
+        tasks.send_invoice_report()
+        message = send_mail_mock.call_args[0][1]
+        lines = message.splitlines()
+        self.assertEqual(2, len(lines))
+
+    @utils.override_invoices_settings(
+        INVOICE_REPORTING=INVOICE_REPORTING,
+        ENABLE_ACCOUNTING_START_DATE=True,
+    )
+    def test_active_customers_are_not_skipped_anyways(self, send_mail_mock):
+        factories.PaymentDetailsFactory(
+            customer=self.customer,
+            accounting_start_date=timezone.now() - datetime.timedelta(days=10)
+        )
+        tasks.send_invoice_report()
+        message = send_mail_mock.call_args[0][1]
+        lines = message.splitlines()
+        self.assertEqual(2, len(lines))
+
+    @utils.override_invoices_settings(
+        INVOICE_REPORTING=INVOICE_REPORTING,
+        ENABLE_ACCOUNTING_START_DATE=True,
+    )
+    def test_empty_invoice_is_skipped(self, send_mail_mock):
+        factories.PaymentDetailsFactory(
+            customer=self.customer,
+            accounting_start_date=timezone.now() - datetime.timedelta(days=10)
+        )
+        for item in self.invoice.items:
+            item.delete()
+
+        tasks.send_invoice_report()
+        message = send_mail_mock.call_args[0][1]
+        lines = message.splitlines()
+        self.assertEqual(0, len(lines))
+
+    @utils.override_invoices_settings(INVOICE_REPORTING=INVOICE_REPORTING)
+    def test_active_invoice_are_merged(self, send_mail_mock):
+        fixture = fixtures.InvoiceFixture()
+        package = fixtures.create_package(111, fixture.openstack_tenant)
+        package.template.name = 'PackageTemplate'
+        package.template.save()
+
+        invoice = models.Invoice.objects.get(customer=package.tenant.service_project_link.project.customer)
+        invoice.customer.agreement_number = 777
+        invoice.customer.save()
+        invoice.set_created()
+
+        tasks.send_invoice_report()
+        message = send_mail_mock.call_args[0][1]
+        lines = message.splitlines()
+        self.assertEqual(3, len(lines))
