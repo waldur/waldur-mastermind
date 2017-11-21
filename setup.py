de89ab841ca2b400cfb939af75c772fbf18dfda5
Jenkins <jenkins@opennodecloud.com>
2017-11-21 15:07:35 +0000

    Preparing new release: 2.8.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requires = [
 
 setup(
     name='waldur-mastermind',
-    version='2.8.0',
+    version='2.8.1',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='http://waldur.com',
