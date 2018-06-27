a36364413ca96adfb38a72ff28efbecf5864ecee
Victor Mireyev <victor@opennodecloud.com>
2018-06-27 16:27:08 +0300

    Add metadata, add user for sharing jUnit logs [WAL-1670]

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
+docker --version
+docker-compose --version
+
 export COMPOSE_PROJECT_NAME=$BUILD_TAG
 docker-compose up --build --detach --no-color
-docker-compose run api waldur-test
+docker-compose run -e LOCAL_USER_ID=`id -u $USER` api waldur-test
 result=$?
-docker-compose down --rmi all &> /dev/null || true &> /dev/null
+docker-compose down &> /dev/null || true &> /dev/null
 exit $result
