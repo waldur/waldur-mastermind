591fbe10ad31c01c64f152340330a7119a694ae3
Jenkins <jenkins@opennodecloud.com>
2018-07-02 15:17:36 +0000

    Preparing new release: 0.8.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_requires = [
 
 setup(
     name='ansible-waldur-module',
-    version='0.8.4',
+    version='0.8.5',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://waldur.com',
