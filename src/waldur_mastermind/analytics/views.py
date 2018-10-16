47ed4431efed8aaa0d8b7764b9d016bcce83acb3
Victor Mireyev <victor@opennodecloud.com>
2018-10-16 11:16:58 -0400

    Reduce number of API requests in project/organization workspace charts [WAL-1886]

---
+++ src/waldur_mastermind/analytics/views.py
@@ -0,0 +1,52 @@
+from __future__ import unicode_literals
+
+import collections
+from datetime import timedelta
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+from . import models, serializers
+
+
+class DailyQuotaHistoryViewSet(viewsets.GenericViewSet):
+    # Fix for schema generation
+    queryset = []
+
+    def list(self, request):
+        serializer = serializers.DailyHistoryQuotaSerializer(
+            data=request.query_params,
+            context={'request': request},
+        )
+        serializer.is_valid(raise_exception=True)
+        result = self.get_result(serializer.validated_data)
+        return Response(result)
+
+    def get_result(self, query):
+        scope = query['scope']
+        quota_names = query['quota_names']
+        start = query['start']
+        end = query['end']
+
+        quotas = models.DailyQuotaHistory.objects.filter(
+            scope=scope,
+            name__in=quota_names,
+            date__gte=start,
+            date__lte=end,
+        ).only(
+            'name',
+            'date',
+            'usage',
+        )
+        charts = collections.defaultdict(dict)
+        for quota in quotas:
+            charts[quota.name][quota.date] = quota.usage
+
+        values = collections.defaultdict(list)
+        day = timedelta(days=1)
+        days = (end - start).days
+        for i in range(days + 1):
+            date = start + i * day
+            for name in quota_names:
+                values[name].append(charts[name].get(date, 0))
+        return values
