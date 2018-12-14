980f90c7a65a5ff8a3a2fe0d3c1142203dda8ace
ilja <ilja@opennodecloud.com>
2018-12-14 12:56:51 +0200

    Expose UUID in Category admin list

--- src/waldur_mastermind/marketplace/admin.py
+++ src/waldur_mastermind/marketplace/admin.py
@@ -52,6 +52,7 @@ class CategoryColumnInline(admin.TabularInline):
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'uuid',)
     inlines = [SectionInline, CategoryColumnInline]
 
 
