a58d6908b8b37179152179346cbf46dfbcf6cafe
Victor Mireyev <victor@opennodecloud.com>
2019-01-23 01:09:47 +0300

    Refactor suport invoices application. Add docstring for proxy model.

--- src/waldur_mastermind/support_invoices/handlers.py
+++ src/waldur_mastermind/support_invoices/handlers.py
@@ -1,10 +1,14 @@
-from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
 
 from waldur_mastermind.invoices import registrators
+from waldur_mastermind.marketplace.models import Resource
 from waldur_mastermind.support import models as support_models
 
 from . import models
+
+
+def is_request_based(offering):
+    return Resource.objects.filter(scope=offering).exists()
 
 
 def add_new_offering_to_invoice(sender, instance, created=False, **kwargs):
@@ -12,22 +16,16 @@ def add_new_offering_to_invoice(sender, instance, created=False, **kwargs):
         return
 
     if instance.tracker.has_changed('state') and instance.state == support_models.Offering.States.OK and \
-            models.RequestBasedOffering.is_request_based(instance):
-        try:
-            request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.pk)
-        except ObjectDoesNotExist:
-            return
+            is_request_based(instance):
+        request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.pk)
         registrators.RegistrationManager.register(request_based_offering, timezone.now())
 
 
 def terminate_invoice_when_offering_deleted(sender, instance, **kwargs):
-    if not models.RequestBasedOffering.is_request_based(instance):
+    if not is_request_based(instance):
         return
 
-    try:
-        request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.pk)
-    except ObjectDoesNotExist:
-        return
+    request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.pk)
     registrators.RegistrationManager.terminate(request_based_offering, timezone.now())
 
 
@@ -35,14 +33,11 @@ def terminate_invoice_when_offering_cancelled(sender, instance, created=False, *
     if created:
         return
 
-    if not models.RequestBasedOffering.is_request_based(instance):
+    if not is_request_based(instance):
         return
 
     if instance.tracker.has_changed('state') and (instance.state == support_models.Offering.States.TERMINATED):
-        try:
-            request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.pk)
-        except ObjectDoesNotExist:
-            return
+        request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.pk)
         registrators.RegistrationManager.terminate(request_based_offering, timezone.now())
 
 
@@ -50,15 +45,12 @@ def switch_plan_resource(sender, instance, created=False, **kwargs):
     if created:
         return
 
-    if not models.RequestBasedOffering.is_request_based(instance.scope):
+    if not isinstance(instance.scope, support_models.Offering):
         return
 
     if not instance.tracker.has_changed('plan_id'):
         return
 
-    try:
-        request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.scope.pk)
-    except ObjectDoesNotExist:
-        return
+    request_based_offering = models.RequestBasedOffering.objects.get(pk=instance.scope.pk)
     registrators.RegistrationManager.terminate(request_based_offering, timezone.now())
     registrators.RegistrationManager.register(request_based_offering, timezone.now())
