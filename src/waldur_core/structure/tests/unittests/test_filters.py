a4b80db6a13b18cae5c7db47026c1664e79d55e9
Faradj Musaev <faradj.musaev@gmail.com>
2019-02-21 08:18:21 -0500

    [WAL-2139] Extend resource usage overview view.

--- src/waldur_core/structure/tests/unittests/test_filters.py
+++ src/waldur_core/structure/tests/unittests/test_filters.py
@@ -1,10 +1,15 @@
-from django.test import TestCase
-from six.moves import mock
+from datetime import datetime, timedelta
 
+from rest_framework.test import APITransactionTestCase
+from six.moves import mock
+from django.test import TestCase
+
+from waldur_core.core.tests.helpers import override_waldur_core_settings
 from waldur_core.logging import models as logging_models
 from waldur_core.logging.tests import factories as logging_factories
 from waldur_core.structure.filters import AggregateFilter
 from waldur_core.structure.tests import factories
+from waldur_core.structure.tests.factories import UserFactory, ProjectFactory
 
 
 class AggregateFilterTest(TestCase):
@@ -63,3 +68,30 @@ class AggregateFilterTest(TestCase):
         }
 
         return self.aggregate_filter.filter(request, self.queryset, None)
+
+
+class CustomerAccountingStartDateFilterTest(APITransactionTestCase):
+
+    def setUp(self):
+        running_customer = factories.CustomerFactory(accounting_start_date=datetime.now() - timedelta(days=7))
+        not_running_customer = factories.CustomerFactory(accounting_start_date=datetime.now() + timedelta(days=7))
+        self.running_project = factories.ProjectFactory(customer=running_customer)
+        self.not_running_project = factories.ProjectFactory(customer=not_running_customer)
+
+    @override_waldur_core_settings(ENABLE_ACCOUNTING_START_DATE=True)
+    def test_accounting_is_running_filter_behaves_properly(self):
+        staff = UserFactory(is_staff=True)
+        self.client.force_authenticate(user=staff)
+
+        response = self.client.get(ProjectFactory.get_list_url())
+        self.assertEqual(len(response.data), 2)
+
+        response = self.client.get(ProjectFactory.get_list_url(), {
+            "accounting_is_running": "true",
+        })
+        self.assertEqual(len(response.data), 1)
+
+        response = self.client.get(ProjectFactory.get_list_url(), {
+            "accounting_is_running": "false",
+        })
+        self.assertEqual(len(response.data), 1)
