3dcf37f17a05160729b359e4cbef8aeba0662da2
Victor Mireyev <victor@opennodecloud.com>
2019-02-27 12:51:31 +0300

    Remove leftover [WAL-2187]

--- src/waldur_mastermind/marketplace_azure/handlers.py
+++ src/waldur_mastermind/marketplace_azure/handlers.py
@@ -12,22 +12,6 @@ from . import utils
 logger = logging.getLogger(__name__)
 
 
-def synchronize_vm(sender, instance, created=False, **kwargs):
-    vm = instance
-    if not created and not vm.tracker.changed('state'):
-        return
-
-    try:
-        resource = marketplace_models.Resource.objects.get(scope=volume)
-    except ObjectDoesNotExist:
-        logger.debug('Skipping resource synchronization for OpenStack volume '
-                     'because marketplace resource does not exist. '
-                     'Resource ID: %s', instance.id)
-        return
-
-    import_resource_metadata(resource)
-
-
 def synchronize_nic(sender, instance, created=False, **kwargs):
     nic = instance
     if not created and not set(nic.tracker.changed()) & {'public_ip_id', 'ip_address'}:
