e77bf88fff1dabaafd50147bc28d443d3a92e5a4
Maxim Prokopenko <maximprokopenko@gmail.com>
2019-04-26 05:44:56 -0400

    [WAL-2242] CRUDing VMs and Volumes in Marketplace.

--- waldur_client.py
+++ waldur_client.py
@@ -61,9 +61,11 @@ class WaldurClient(object):
         Tenant = 'openstack-tenants'
         TenantSecurityGroup = 'openstack-security-groups'
         Volume = 'openstacktenant-volumes'
+        OpenStackPackage = 'openstack-packages'
         MarketplaceOffering = 'marketplace-offerings'
         MarketplacePlan = 'marketplace-plans'
         MarketplaceOrder = 'marketplace-orders'
+        MarketplaceResources = 'marketplace-resources'
 
     def __init__(self, api_url, access_token):
         """
@@ -125,24 +127,43 @@ class WaldurClient(object):
     def _delete(self, url, valid_states, **kwargs):
         return self._make_request('delete', url, valid_states, **kwargs)
 
-    def _query_resource(self, endpoint, query_params, get_first=False):
-        url = self._build_url(endpoint)
-        if 'uuid' in query_params:
-            url += query_params.pop('uuid') + '/'
+    def _make_get_query(self, url, query_params, get_first=False, get_few=False):
+        """
+        Get object via Waldur API.
+
+        :param url: URL.
+        :param query_params: dict with query params.
+        :param get_first: If True then will return the first result.
+        :param get_few: If True then will return all results.
+
+        Note:
+        If get_first or get_few have been set, then multiple results are correct.
+        In the first case, we get the first result, in the second case we get all results.
+        If get_first or get_few have not been set, then multiple results are not correct."""
 
         result = self._get(url, valid_states=[200], params=query_params)
         if not result:
-            message = 'Result is empty. Endpoint: %s. Query: %s' % (endpoint, query_params)
+            message = 'Result is empty. Endpoint: %s. Query: %s' % (url, query_params)
             raise ObjectDoesNotExist(message)
 
         if isinstance(result, dict):
             return result
 
-        if len(result) > 1 and not get_first:
-            message = 'Ambiguous result. Endpoint: %s. Query: %s' % (url, query_params)
-            raise MultipleObjectsReturned(message)
+        if len(result) > 1:
+            if not get_first and not get_few:
+                message = 'Ambiguous result. Endpoint: %s. Query: %s' % (url, query_params)
+                raise MultipleObjectsReturned(message)
+            elif get_few:
+                return result
 
-        return result[0]
+        return result if get_few else result[0]
+
+    def _query_resource(self, endpoint, query_params, get_first=False):
+        url = self._build_url(endpoint)
+        if 'uuid' in query_params:
+            url += query_params.pop('uuid') + '/'
+
+        return self._make_get_query(url, query_params, get_first)
 
     def _query_resource_by_uuid(self, endpoint, value, extra=None):
         payload = {'uuid': value}
@@ -520,7 +541,77 @@ class WaldurClient(object):
             return self._query_resource(endpoint, query)
 
     def get_instance(self, name, project=None):
+        """
+        Deprecated. Use get_instance_via_marketplace marketplace method"""
         return self._get_project_resource(self.Endpoints.Instance, name, project)
+
+    def get_marketplace_resource_scope(self, name, offering_type, project=None):
+        """Get marketplace resource scope. Depending on the offering type scope type can be different.
+
+            :param name: name of the scope.
+            :param offering_type: marketplace offering type.
+            :param project: project UUID or name.
+        """
+
+        if not is_uuid(name) and not project:
+            raise ValidationError("You should specify project name if name is not UUID")
+
+        endpoint = self.Endpoints.MarketplaceResources
+        url = self._build_url(endpoint)
+        params = {'offering_type': offering_type}
+
+        if project:
+            if is_uuid(project):
+                params['project_uuid'] = project
+            else:
+                params['project_name'] = project
+
+        result = self._get(url, valid_states=[200], params=params)
+
+        if not result:
+            message = 'Result is empty. Endpoint: %s. Query: %s' % (endpoint, params)
+            raise ObjectDoesNotExist(message)
+
+        scope = None
+        resource = None
+
+        for r in result:
+            scope_url = r['scope']
+            scope_result = self._get(scope_url, valid_states=[200])
+
+            if (not is_uuid(name) and scope_result['name'] == name) or \
+                    (is_uuid(name) and scope_result['uuid'] == name):
+
+                if not scope:
+                    scope = scope_result
+                    resource = r
+                else:
+                    message = 'Ambiguous result. Endpoint: %s. Query: %s' % (url, params)
+                    raise MultipleObjectsReturned(message)
+
+        if not scope:
+            message = 'Result is empty. Endpoint: %s. Query: %s' % (endpoint, params)
+            raise ObjectDoesNotExist(message)
+
+        return resource, scope
+
+    def get_instance_via_marketplace(self, name, project=None):
+        """Get an openstack instance via marketplace.
+
+            :param name: name of the instance.
+            :param project: project UUID or name.
+        """
+        resource, instance = self.get_marketplace_resource_scope(name, 'OpenStackTenant.Instance', project)
+        return instance
+
+    def get_volume_via_marketplace(self, name, project=None):
+        """Get an openstack volume via marketplace.
+
+            :param name: name of the volume.
+            :param project: project UUID or name.
+        """
+        resource, instance = self.get_marketplace_resource_scope(name, 'OpenStackTenant.Volume', project)
+        return instance
 
     def stop_instance(self, uuid, wait=True, interval=10, timeout=600):
         """
@@ -751,7 +842,7 @@ class WaldurClient(object):
     def _get_plan(self, identifier):
         return self._get_resource(self.Endpoints.MarketplacePlan, identifier)
 
-    def create_marketplace_order(self, project, offering, plan, attributes=None, limits=None):
+    def create_marketplace_order(self, project, offering, plan=None, attributes=None, limits=None):
         """
         Create order with one item in Waldur Marketplace.
 
@@ -763,19 +854,243 @@ class WaldurClient(object):
         """
         project_url = self._get_project(project)['url']
         offering_url = self._get_offering(offering)['url']
-        plan_url = self._get_plan(plan)['url']
+
         attributes = attributes or {}
         limits = limits or {}
-        payload = {
-            'project': project_url,
-            'items': [{
+        order_item = {
                 'offering': offering_url,
-                'plan': plan_url,
                 'attributes': attributes,
                 'limits': limits,
-            }],
+            }
+
+        if plan:
+            plan_url = self._get_plan(plan)['url']
+            order_item['plan'] = plan_url
+
+        payload = {
+            'project': project_url,
+            'items': [order_item],
         }
         return self._create_resource(self.Endpoints.MarketplaceOrder, payload=payload)
+
+    def approve_marketplace_order(self, order_uuid):
+        """
+        Approve marketplace order.
+
+        :param order_uuid: order UUID
+        """
+        url = self._build_resource_url(self.Endpoints.MarketplaceOrder, order_uuid, action='approve')
+        return self._post(url, valid_states=[200])
+
+    def _create_scope_via_marketplace(
+            self,
+            name,
+            offering,
+            project,
+            attributes,
+            offering_type,
+            scope_endpoint,
+            interval=10,
+            timeout=600,
+            wait=True,
+            check_mode=False):
+        offering = self._get_resource(self.Endpoints.MarketplaceOffering, offering)
+        
+        if check_mode:
+            return {
+                'attributes': attributes,
+                'project': project,
+                'offering': offering['uuid'],
+                'WALDUR_CHECK_MODE': True
+            }
+
+        order = self.create_marketplace_order(project, offering['uuid'], attributes=attributes)
+        self.approve_marketplace_order(order['uuid'])
+        scope = None
+        waited = 0
+        while not scope:
+            time.sleep(interval)
+            try:
+                resource, scope = self.get_marketplace_resource_scope(name, offering_type, project)
+            except ObjectDoesNotExist:
+                pass
+            waited += interval
+            if waited >= timeout:
+                error = 'Marketplace resource of scope with name "%s" is not found.' % name
+                message = '%s. Seconds passed: %s' % (error, timeout)
+                raise TimeoutError(message)
+
+        if wait:
+            self._wait_for_resource(scope_endpoint, scope['uuid'], interval, timeout)
+
+        return scope
+
+    def create_instance_via_marketplace(
+            self,
+            name,
+            offering,
+            project,
+            networks,
+            image,
+            system_volume_size,
+            description=None,
+            flavor=None,
+            flavor_min_cpu=None,
+            flavor_min_ram=None,
+            interval=10,
+            timeout=600,
+            wait=True,
+            ssh_key=None,
+            data_volume_size=None,
+            security_groups=None,
+            tags=None,
+            user_data=None,
+            check_mode=False):
+        """
+        Create OpenStack instance from passed parameters via marketplace.
+
+        :param name: name of the instance.
+        :param description: description of the instance.
+        :param provider: uuid or name of the provider to use.
+        :param project: uuid or name of the project to add the instance.
+        :param networks: a list of networks to attach instance to.
+        :param flavor: uuid or name of the flavor to use.
+        :param flavor_min_cpu: min cpu count.
+        :param flavor_min_ram: min ram size (MB).
+        :param image: uuid or name of the image to use.
+        :param system_volume_size: size of the system volume in GB.
+        :param interval: interval of instance state polling in seconds.
+        :param timeout: a maximum amount of time to wait for instance provisioning.
+        :param wait: defines whether the client has to wait for instance provisioning.
+        :param ssh_key: uuid or name of the ssh key to add to the instance.
+        :param data_volume_size: size of the data volume in GB.
+            No data volume is going to be created if empty.
+        :param security_groups: list of security groups to add to the instance.
+        :param tags: list of tags to add to the instance.
+        :param user_data: additional data that will be added to the instance.
+        :return: an instance as a dictionary.
+        """
+        offering = self._get_resource(self.Endpoints.MarketplaceOffering, offering)
+        settings_uuid = offering['scope_uuid']
+
+        # Collect attributes
+        if flavor:
+            flavor = self._get_flavor(flavor, settings_uuid)
+        else:
+            flavor = self._get_flavor_from_params(flavor_min_cpu, flavor_min_ram)
+
+        image = self._get_image(image, settings_uuid)
+        subnets, floating_ips = self._networks_to_payload(networks)
+
+        attributes = {
+            'name': name,
+            'flavor': flavor['url'],
+            'image': image['url'],
+            'system_volume_size': system_volume_size * 1024,
+            'internal_ips_set': subnets,
+            'floating_ips': floating_ips,
+        }
+
+        if security_groups:
+            attributes['security_groups'] = []
+            for group in security_groups:
+                security_group = self._get_security_group(group, settings_uuid)
+                attributes['security_groups'].append({'url': security_group['url']})
+
+        if data_volume_size:
+            attributes.update({'data_volume_size': data_volume_size * 1024})
+        if user_data:
+            attributes.update({'user_data': user_data})
+        if ssh_key:
+            ssh_key = self._get_resource(self.Endpoints.SshKey, ssh_key)
+            attributes.update({'ssh_public_key': ssh_key['url']})
+        if description:
+            attributes['description'] = description
+
+        instance = self._create_scope_via_marketplace(
+            name,
+            offering['uuid'],
+            project,
+            attributes,
+            offering_type='OpenStackTenant.Instance',
+            scope_endpoint=self.Endpoints.Instance,
+            interval=interval,
+            timeout=timeout,
+            wait=wait,
+            check_mode=check_mode)
+
+        if wait and floating_ips:
+            self._wait_for_external_ip(instance['uuid'], interval, timeout)
+
+        return instance
+
+    def _delete_scope_via_marketplace(self, scope_uuid, offering_type):
+        resource, scope = self.get_marketplace_resource_scope(scope_uuid, offering_type)
+        url = self._build_resource_url(self.Endpoints.MarketplaceResources, resource['uuid'], action='terminate')
+        order_uuid = self._post(url, valid_states=[200])['order_uuid']
+        return order_uuid
+
+    def delete_instance_via_marketplace(self, instance_uuid):
+        """
+        Delete OpenStack instance via marketplace.
+
+        :param instance_uuid: instance UUID.
+        """
+        return self._delete_scope_via_marketplace(instance_uuid, 'OpenStackTenant.Instance')
+
+    def create_volume_via_marketplace(self,
+                      name,
+                      project,
+                      offering,
+                      size,
+                      description=None,
+                      tags=None,
+                      wait=True,
+                      interval=10,
+                      timeout=600):
+        """
+        Create OpenStack volume from passed parameters via marketplace.
+
+        :param name: name of the volume.
+        :param project: uuid or name of the project to add the volume to.
+        :param provider: uuid or name of the provider to use.
+        :param size: size of the volume in GBs.
+        :param description: arbitrary text.
+        :param tags: list of tags to add to the volume.
+        :param wait: defines whether the client has to wait for volume provisioning.
+        :param interval: interval of volume state polling in seconds.
+        :param timeout: a maximum amount of time to wait for volume provisioning.
+        :return: volume as a dictionary.
+        """
+
+        # Collect attributes
+        attributes = {
+            'name': name,
+            'size': size * 1024,
+        }
+        if description:
+            attributes.update({'description': description})
+        if tags:
+            attributes.update({'tags': tags})
+
+        return self._create_scope_via_marketplace(
+            name,
+            offering,
+            project,
+            attributes,
+            offering_type='OpenStackTenant.Volume',
+            scope_endpoint=self.Endpoints.Volume,
+            interval=interval,
+            timeout=timeout,
+            wait=wait)
+
+    def delete_volume_via_marketplace(self, volume_uuid):
+        """
+        Delete OpenStack volume via marketplace.
+
+        :param volume_uuid: volume UUID.
+        """
+        return self._delete_scope_via_marketplace(volume_uuid, 'OpenStackTenant.Volume')
 
 
 def waldur_full_argument_spec(**kwargs):
