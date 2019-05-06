4f7880fdfc8fedb7e4d9251131ecaaaedffda8fd
mprokopenko <maximprokopenko@gmail.com>
2019-05-06 10:03:41 +0300

    [WAL-2366] Add a test.

--- src/waldur_openstack/openstack_tenant/backend.py
+++ src/waldur_openstack/openstack_tenant/backend.py
@@ -484,8 +484,8 @@ class OpenStackTenantBackend(BaseOpenStackBackend):
         if volume.type:
             kwargs['volume_type'] = volume.type.backend_id
         else:
-            volume_type_name = volume.service_project_link.service.settings.scope and \
-                               volume.service_project_link.service.settings.scope.default_volume_type_name
+            volume_type_name = (volume.service_project_link.service.settings.scope and
+                                volume.service_project_link.service.settings.scope.default_volume_type_name)
             if volume_type_name:
                 try:
                     volume_type = models.VolumeType.objects.get(name=volume_type_name,
