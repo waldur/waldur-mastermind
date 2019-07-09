e36caab07168d3884b55970e5fa6ee5146df1b1c
Maanus <maanus@leesment.com>
2019-07-09 17:50:43 +0300

    Fix using mock in production [WAL-2530]

--- src/waldur_mastermind/booking/processors.py
+++ src/waldur_mastermind/booking/processors.py
@@ -1,4 +1,29 @@
-import mock
+from waldur_mastermind.marketplace import processors
 
-BookingCreateProcessor = mock.MagicMock()
-BookingDeleteProcessor = mock.MagicMock()
+
+class BookingCreateProcessor(processors.CreateResourceProcessor):
+    def get_serializer_class(self):
+        pass
+
+    def get_viewset(self):
+        pass
+
+    def get_post_data(self):
+        pass
+
+    def get_scope_from_response(self, response):
+        pass
+
+
+class BookingDeleteProcessor(processors.CreateResourceProcessor):
+    def get_viewset(self):
+        pass
+
+    def get_post_data(self):
+        pass
+
+    def get_scope_from_response(self, response):
+        pass
+
+    def get_serializer_class(self):
+        pass
