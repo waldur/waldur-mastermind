af028ac8105855998c311fcd6d5ba2a8b6832675
Victor Mireyev <victor@opennodecloud.com>
2019-07-18 21:05:11 +0300

    Emit resource creation event only after transaction has been committed.

--- src/waldur_core/structure/handlers.py
+++ src/waldur_core/structure/handlers.py
@@ -4,6 +4,7 @@ import logging
 import re
 
 from django.conf import settings
+from django.db import transaction
 from django.template.loader import render_to_string
 from django.utils import timezone
 
@@ -241,11 +242,15 @@ def log_resource_creation_failed(instance):
 
 def log_resource_creation_scheduled(sender, instance, created=False, **kwargs):
     if created and isinstance(instance, StateMixin) and instance.state == StateMixin.States.CREATION_SCHEDULED:
-        event_logger.resource.info(
-            'Resource {resource_name} creation has been scheduled.',
-            event_type='resource_creation_scheduled',
-            event_context={'resource': instance},
-        )
+        transaction.on_commit(_log_resource_creation_scheduled, instance)
+
+
+def _log_resource_creation_scheduled(instance):
+    event_logger.resource.info(
+        'Resource {resource_name} creation has been scheduled.',
+        event_type='resource_creation_scheduled',
+        event_context={'resource': instance},
+    )
 
 
 def log_resource_action(sender, instance, name, source, target, **kwargs):
