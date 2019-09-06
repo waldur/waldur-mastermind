75f2daf6912fb4c669f61e1543c7ad6d62c02a5f
Maxim Prokopenko <maximprokopenko@gmail.com>
2019-09-06 01:48:26 -0400

    [WAL-2616] State machine error messages should use proper state names.

--- src/waldur_core/core/monkeypatch.py
+++ src/waldur_core/core/monkeypatch.py
@@ -30,6 +30,62 @@ def patch_field_descriptor(cls):
     setattr(cls, 'field_name', property(get_field_name))
 
 
+def patch_fsm_field_mixin(cls):
+    from django_fsm import TransitionNotAllowed, pre_transition, post_transition
+
+    def change_state(self, instance, method, *args, **kwargs):
+        meta = method._django_fsm
+        method_name = method.__name__
+        current_state = self.get_state(instance)
+        try:
+            current_state_name = filter(lambda x: x[0] == current_state, meta.field.choices)[0][1]
+        except Exception:
+            current_state_name = current_state
+
+        if not meta.has_transition(current_state):
+            raise TransitionNotAllowed(
+                "Can't switch from state '{0}' using method '{1}'".format(current_state_name, method_name),
+                object=instance, method=method)
+        if not meta.conditions_met(instance, current_state):
+            raise TransitionNotAllowed(
+                "Transition conditions have not been met for method '{0}'".format(method_name),
+                object=instance, method=method)
+
+        next_state = meta.next_state(current_state)
+
+        signal_kwargs = {
+            'sender': instance.__class__,
+            'instance': instance,
+            'name': method_name,
+            'source': current_state,
+            'target': next_state
+        }
+
+        pre_transition.send(**signal_kwargs)
+
+        try:
+            result = method(instance, *args, **kwargs)
+            if next_state is not None:
+                self.set_proxy(instance, next_state)
+                self.set_state(instance, next_state)
+        except Exception as exc:
+            exception_state = meta.exception_state(current_state)
+            if exception_state:
+                self.set_proxy(instance, exception_state)
+                self.set_state(instance, exception_state)
+                signal_kwargs['target'] = exception_state
+                signal_kwargs['exception'] = exc
+                post_transition.send(**signal_kwargs)
+            raise
+        else:
+            post_transition.send(**signal_kwargs)
+
+        return result
+
+    cls.change_state = change_state
+
+
 def monkey_patch_fields():
-    from django_fsm import FSMFieldDescriptor
+    from django_fsm import FSMFieldDescriptor, FSMFieldMixin
     patch_field_descriptor(FSMFieldDescriptor)
+    patch_fsm_field_mixin(FSMFieldMixin)
