7fb1d4048e80900205319cdba669bef0bd6672bf
Victor Mireyev <victor@opennodecloud.com>
2019-09-09 22:03:29 +0300

    Drop data migrations involving generic invoice items.

--- src/waldur_mastermind/invoices/migrations/0027_migrate_items.py
+++ src/waldur_mastermind/invoices/migrations/0027_migrate_items.py
@@ -4,56 +4,6 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
-def migrate_items(apps, schema_editor):
-    OpenStackItem = apps.get_model('invoices', 'OpenStackItem')
-    OfferingItem = apps.get_model('invoices', 'OfferingItem')
-    GenericInvoiceItem = apps.get_model('invoices', 'GenericInvoiceItem')
-    Offering = apps.get_model('support', 'Offering')
-    OpenStackPackage = apps.get_model('packages', 'OpenStackPackage')
-
-    ContentType = apps.get_model('contenttypes', 'ContentType')
-    offering_ct = ContentType.objects.get_for_model(Offering)
-    package_ct = ContentType.objects.get_for_model(OpenStackPackage)
-
-    for item in OpenStackItem.objects.all():
-        generic_item = GenericInvoiceItem.objects.create(
-            invoice=item.invoice,
-            details=item.package_details,
-            start=item.start,
-            end=item.end,
-            project=item.project,
-            project_name=item.project_name,
-            project_uuid=item.project_uuid,
-            unit_price=item.unit_price,
-            unit=item.unit,
-            product_code=item.product_code,
-            article_code=item.article_code
-        )
-        if item.package:
-            generic_item.content_type = package_ct
-            generic_item.object_id = item.package.id
-            generic_item.save()
-
-    for item in OfferingItem.objects.all():
-        generic_item = GenericInvoiceItem.objects.create(
-            invoice=item.invoice,
-            details=item.offering_details,
-            start=item.start,
-            end=item.end,
-            project=item.project,
-            project_name=item.project_name,
-            project_uuid=item.project_uuid,
-            unit_price=item.unit_price,
-            unit=item.unit,
-            product_code=item.product_code,
-            article_code=item.article_code
-        )
-        if item.offering:
-            generic_item.content_type = offering_ct
-            generic_item.object_id = item.offering.id
-            generic_item.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -61,6 +11,4 @@ class Migration(migrations.Migration):
         ('contenttypes', '0002_remove_content_type_name'),
     ]
 
-    operations = [
-        migrations.RunPython(migrate_items),
-    ]
+    operations = []
--- src/waldur_mastermind/invoices/migrations/0029_migrate_invoice_item_name.py
+++ src/waldur_mastermind/invoices/migrations/0029_migrate_invoice_item_name.py
@@ -4,34 +4,6 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 
-def migrate_items(apps, schema_editor):
-    ContentType = apps.get_model('contenttypes', 'ContentType')
-    GenericInvoiceItem = apps.get_model('invoices', 'GenericInvoiceItem')
-
-    Offering = apps.get_model('support', 'Offering')
-    OpenStackPackage = apps.get_model('packages', 'OpenStackPackage')
-
-    offering_ct = ContentType.objects.get_for_model(Offering)
-    package_ct = ContentType.objects.get_for_model(OpenStackPackage)
-
-    for item in GenericInvoiceItem.objects.filter(object_id=None):
-        if item.details.get('tenant_name') and item.details.get('tenant_uuid'):
-            item.content_type = package_ct
-            item.details.update({
-                'name': item.details['tenant_name'],
-                'scope_uuid': item.details['tenant_uuid'],
-            })
-            item.save()
-
-        elif item.details.get('offering_name') and item.details.get('offering_uuid'):
-            item.content_type = offering_ct
-            item.details.update({
-                'name': item.details['offering_name'],
-                'scope_uuid': item.details['offering_uuid'],
-            })
-            item.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -40,6 +12,4 @@ class Migration(migrations.Migration):
         ('invoices', '0028_delete_offering_and_openstack_item'),
     ]
 
-    operations = [
-        migrations.RunPython(migrate_items),
-    ]
+    operations = []
