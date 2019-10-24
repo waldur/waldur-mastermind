8d89666714dfde23350b136e4380c1fd9078b79c
Victor Mireyev <victor@opennodecloud.com>
2019-10-24 08:22:12 +0300

    Fix mocking.

--- src/waldur_paypal/tests/unittests/test_backend.py
+++ src/waldur_paypal/tests/unittests/test_backend.py
@@ -66,7 +66,7 @@ class DownloadInvoicePDFTest(BaseBackendTest):
 
         self.assertRaises(PayPalError, self.backend.download_invoice_pdf, self.invoice)
 
-    @mock.patch('waldur_paypal.backend.urllib2.urlopen')
+    @mock.patch('waldur_paypal.backend.urlopen')
     def test_pdf_is_downloaded(self, urlopen_mock):
         response = mock.Mock()
         response.read.return_value = 'PDF file content'
