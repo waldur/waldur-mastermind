655380e4e55ee041af94645a948ac4045e435224
Victor Mireyev <victor@opennodecloud.com>
2019-11-06 12:48:52 +0300

    Temporarily disable default integrations for Sentry so that it wouldn't add extra HTTP headers to requests.

--- packaging/settings.py
+++ packaging/settings.py
@@ -351,7 +351,8 @@ if config.get('sentry', 'dsn') != '':
 
     sentry_sdk.init(
         dsn=config.get('sentry', 'dsn'),
-        integrations=[DjangoIntegration()]
+        integrations=[DjangoIntegration()],
+        default_integrations=False,
     )
 
 # Additional configuration files for Waldur
