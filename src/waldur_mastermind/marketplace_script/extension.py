dbc5068091fdbdf09dc5adbcf6b058e87c9a842e
Victor Mireyev <victor@opennodecloud.com>
2019-11-22 20:01:56 +0300

    Add documentation links.

--- src/waldur_mastermind/marketplace_script/extension.py
+++ src/waldur_mastermind/marketplace_script/extension.py
@@ -5,12 +5,15 @@ class MarketplaceScriptExtension(WaldurExtension):
 
     class Settings:
         WALDUR_MARKETPLACE_SCRIPT = {
+            # See also: https://docker-py.readthedocs.io/en/stable/client.html#docker.client.DockerClient
             'DOCKER_CLIENT': {
                 'base_url': None,
             },
+            # See also: https://docker-py.readthedocs.io/en/stable/containers.html#docker.models.containers.ContainerCollection.run
             'DOCKER_RUN_OPTIONS': {
                 'mem_limit': '64m',
             },
+            # Key is image name, value is command to execute script.
             'DOCKER_IMAGES': {
                 'python:3.7-alpine': 'python',
                 'alpine:3.10.0': 'sh',
