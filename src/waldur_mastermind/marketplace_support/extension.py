4542c3aad45bc5f95c69defbf6c5ede4beedb47f
Maxim Prokopenko <maximprokopenko@gmail.com>
2019-12-05 03:51:11 -0500

    [WAL-2712] Expose plan and Marketplace resource UUID in ticket.

--- src/waldur_mastermind/marketplace_support/extension.py
+++ src/waldur_mastermind/marketplace_support/extension.py
@@ -6,15 +6,21 @@ class MarketplaceSupportExtension(WaldurExtension):
         WALDUR_MARKETPLACE_SUPPORT = {
             'REQUEST_LINK_TEMPLATE': 'https://www.example.com/#/offering/{request_uuid}/',
             'CREATE_RESOURCE_TEMPLATE': (
+                '{% load waldur_marketplace %}'
                 '\n[Order item|{{order_item_url}}].'
-                '\nVendor: {{order_item.offering.customer.name}}'
+                '\nProvider: {{order_item.offering.customer.name}}'
+                '\nPlan details:\n{% plan_details order_item.plan %}'
             ),
             'UPDATE_RESOURCE_TEMPLATE': (
                 '\n[Switch plan for resource {{order_item.resource.scope.name}}|{{request_url}}].'
                 '\nSwitch from {{order_item.resource.plan.name}} plan to {{order_item.plan.name}}.'
+                '\nMarketplace resource UUID: {{order_item.resource.uuid.hex}}'
             ),
             'TERMINATE_RESOURCE_TEMPLATE': (
+                '{% load waldur_marketplace %}'
                 '[Terminate resource {{order_item.resource.scope.name}}|{{request_url}}].'
+                '\n{% plan_details order_item.resource.plan %}'
+                '\nMarketplace resource UUID: {{order_item.resource.uuid.hex}}'
             ),
         }
 
