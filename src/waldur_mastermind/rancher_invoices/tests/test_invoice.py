a5ebeeab0f12c800a71037a67beafad6a5be6a07
Victor Mireyev <victor@opennodecloud.com>
2019-12-06 23:57:48 +0300

    Fix tests.

--- src/waldur_mastermind/rancher_invoices/tests/test_invoice.py
+++ src/waldur_mastermind/rancher_invoices/tests/test_invoice.py
@@ -68,7 +68,7 @@ class InvoiceTest(test.APITransactionTestCase):
             attributes={'name': 'name',
                         'nodes': [{
                             'subnet': openstack_tenant_factories.SubNetFactory.get_url(self.fixture.subnet),
-                            'storage': 1024,
+                            'system_volume_size': 1024,
                             'memory': 1,
                             'cpu': 1,
                             'roles': ['controlplane', 'etcd', 'worker']
