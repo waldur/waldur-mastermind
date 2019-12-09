a3b1e57194ae3891834b3ef9649a58bdbde7f875
mprokopenko <maximprokopenko@gmail.com>
2019-12-09 13:30:24 +0300

    [WAL-2706] Delete a method.

--- src/waldur_jira/serializers.py
+++ src/waldur_jira/serializers.py
@@ -475,7 +475,7 @@ class WebHookReceiverSerializer(serializers.Serializer):
         if event_type in self.Event.ISSUE_ACTIONS:
             if not issue and create_issue:
                 backend.create_issue_from_jira(project, key)
-                self._update_attachments(backend, issue)
+                backend.update_attachment_from_jira(issue)
 
             if event_type == self.Event.ISSUE_UPDATE:
                 if old_jira:
@@ -500,8 +500,8 @@ class WebHookReceiverSerializer(serializers.Serializer):
                         backend.update_issue_from_jira(issue)
 
                 else:
-                    self._update_attachments(backend, issue)
                     backend.update_issue_from_jira(issue)
+                    backend.update_attachment_from_jira(issue)
 
             if event_type == self.Event.ISSUE_DELETE:
                 backend.delete_issue_from_jira(issue)
@@ -517,16 +517,13 @@ class WebHookReceiverSerializer(serializers.Serializer):
 
             if not comment and create_comment:
                 backend.create_comment_from_jira(issue, comment_backend_id)
-                self._update_attachments(backend, issue)
+                backend.update_attachment_from_jira(issue)
 
             if event_type == self.Event.COMMENT_UPDATE:
                 backend.update_comment_from_jira(comment)
-                self._update_attachments(backend, issue)
+                backend.update_attachment_from_jira(issue)
 
             if event_type == self.Event.COMMENT_DELETE:
                 backend.delete_comment_from_jira(comment)
 
         return validated_data
-
-    def _update_attachments(self, backend, issue):
-        backend.update_attachment_from_jira(issue)
