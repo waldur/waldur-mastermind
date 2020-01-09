44091da7c730a7cf66c825f1b92a6a971e8228a7
Victor Mireyev <victor@opennodecloud.com>
2020-01-09 14:26:32 +0300

    Skip marketplace resource creation when cluster is imported directly [WAL-2768]

--- src/waldur_mastermind/marketplace_rancher/handlers.py
+++ src/waldur_mastermind/marketplace_rancher/handlers.py
@@ -3,6 +3,10 @@ from waldur_mastermind.marketplace.utils import get_resource_state
 
 
 def create_marketplace_resource_for_imported_cluster(sender, instance, offering=None, plan=None, **kwargs):
+    if not offering or not plan:
+        # When cluster is imported directly (ie without marketplace),
+        # marketplace resources are not created.
+        return
     resource = marketplace_models.Resource(
         project=instance.service_project_link.project,
         state=get_resource_state(instance.state),
