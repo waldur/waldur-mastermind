e33c446546306edb73bbac7fb97758eef91b75c6
mprokopenko <maximprokopenko@gmail.com>
2020-01-22 00:37:01 +0300

    [WAL-2781] Migration fix.

--- src/waldur_mastermind/marketplace/migrations/0011_offering_datacite_doi.py
+++ src/waldur_mastermind/marketplace/migrations/0011_offering_datacite_doi.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('marketplace', '0009_offering_parent'),
+        ('marketplace', '0010_allow_null_widget'),
     ]
 
     operations = [
