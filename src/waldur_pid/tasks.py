3c84a0ecee3af844c9d47497894db723b98f104f
mprokopenko <maximprokopenko@gmail.com>
2020-01-23 14:24:58 +0300

    [WAL-2781] Add a check.

--- src/waldur_pid/tasks.py
+++ src/waldur_pid/tasks.py
@@ -11,6 +11,12 @@ logger = logging.getLogger(__name__)
 @shared_task
 def create_doi(serialized_instance):
     instance = core_utils.deserialize_instance(serialized_instance)
+
+    if instance.datacite_doi:
+        logger.warning(
+            'Registration of %s has been skipped because datacite_doi field is not empty.' % instance)
+        return
+
     try:
         backend.DataciteBackend().create_doi(instance)
     except exceptions.DataciteException as e:
