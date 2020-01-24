ed426f281d08cc203a4b1a80c474ba854d3854bc
mprokopenko <maximprokopenko@gmail.com>
2020-01-24 16:11:22 +0300

    [WAL-2797] Add polling of node state.

--- src/waldur_rancher/executors.py
+++ src/waldur_rancher/executors.py
@@ -3,7 +3,29 @@ from celery import chain
 from waldur_core.core import executors as core_executors
 from waldur_core.core import tasks as core_tasks, utils as core_utils
 
-from . import tasks
+from . import tasks, models
+
+
+class PollNodeStateTask(core_tasks.Task):
+    max_retries = 1200
+    default_retry_delay = 5
+
+    @classmethod
+    def get_description(cls, instance, backend_pull_method, *args, **kwargs):
+        return 'Poll node "%s" with method "%s"' % (instance, backend_pull_method)
+
+    def get_backend(self, instance):
+        return instance.get_backend()
+
+    def execute(self, instance, backend_pull_method):
+        backend = self.get_backend(instance)
+        getattr(backend, backend_pull_method)(instance)
+        instance.refresh_from_db()
+
+        if not instance.runtime_state:
+            self.retry()
+
+        return instance
 
 
 class ClusterCreateExecutor(core_executors.CreateExecutor):
@@ -25,6 +47,17 @@ class ClusterCreateExecutor(core_executors.CreateExecutor):
             _tasks.append(tasks.CreateNodeTask().si(
                 serialized_instance,
                 user_id=user.id,
+            ))
+
+        for node in nodes:
+            serialized_instance = core_utils.serialize_instance(node)
+            _tasks.append(tasks.CreateNodeTask().si(
+                serialized_instance,
+                user_id=user.id,
+            ))
+            _tasks.append(PollNodeStateTask().si(
+                serialized_instance,
+                backend_pull_method='update_node_details',
             ))
         return _tasks
 
