901904073e7d580130784634c16e768a1ad212fa
Victor Mireyev <victor@opennodecloud.com>
2020-03-16 18:40:59 +0300

    Remove leftover.

--- src/waldur_cost_planning/apps.py
+++ src/waldur_cost_planning/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class CostPlanningConfig(AppConfig):
     name = 'waldur_cost_planning'
     verbose_name = 'Cost planning'
-
-    def ready(self):
-        from .plugins import digitalocean, openstack_tenant, aws  # noqa: F401
