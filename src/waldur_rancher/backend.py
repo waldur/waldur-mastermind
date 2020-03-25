9de43ca3984c038bca3c2cad0bbf6ef3d1c675b6
Victor Mireyev <victor@opennodecloud.com>
2020-03-25 13:52:00 +0300

    Fix Rancher resource pulling: use remote ID instead of local ID [WAL-2955]

--- src/waldur_rancher/backend.py
+++ src/waldur_rancher/backend.py
@@ -504,7 +504,7 @@ class RancherBackend(ServiceBackend):
             update_pulled_fields(local_project, remote_project, pulled_fields)
 
         models.Project.objects.bulk_create(new_projects)
-        local_projects.filter(id__in=stale_projects).delete()
+        local_projects.filter(backend_id__in=stale_projects).delete()
 
     def remote_project_to_local(self, remote_project, local_cluster_map):
         return models.Project(
@@ -559,7 +559,7 @@ class RancherBackend(ServiceBackend):
             update_pulled_fields(local_namespace, remote_namespace, pulled_fields)
 
         models.Namespace.objects.bulk_create(new_namespaces)
-        local_namespaces.filter(id__in=stale_namespaces).delete()
+        local_namespaces.filter(backend_id__in=stale_namespaces).delete()
 
     def remote_namespace_to_local(self, remote_namespace, local_project_map):
         return models.Namespace(
@@ -619,7 +619,7 @@ class RancherBackend(ServiceBackend):
             update_pulled_fields(local_template, remote_template, pulled_fields)
 
         models.Template.objects.bulk_create(new_templates)
-        local_templates.filter(id__in=stale_templates).delete()
+        local_templates.filter(backend_id__in=stale_templates).delete()
 
     def remote_template_to_local(
         self, remote_template, local_catalog_map, local_cluster_map, local_project_map
