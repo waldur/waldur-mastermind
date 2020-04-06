7674c27bab15e388735c811f73095ebaa709b27e
Victor Mireyev <victor@opennodecloud.com>
2020-04-06 18:17:20 +0300

    Drop leftover plugins.

--- src/waldur_ansible/common/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnsibleCommonConfig(AppConfig):
-    name = 'waldur_ansible.common'
-    verbose_name = 'Waldur Ansible Common'
--- src/waldur_ansible/common/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-
-
-def register_in(router):
-    router.register(
-        r'applications', views.ApplicationsSummaryViewSet, basename='applications'
-    )
