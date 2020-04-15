702e099a8ff47d3eacb6beccb8bc9d8039e71ba2
Maxim Prokopenko <maxim.prokopenko@gmail.com>
2020-04-15 14:04:23 +0300

    [WAL-3000] Add tests.

--- src/waldur_mastermind/invoices/tests/test_profiles.py
+++ src/waldur_mastermind/invoices/tests/test_profiles.py
@@ -16,6 +16,9 @@ class ProfileRetrieveTest(test.APITransactionTestCase):
             organization=self.fixture.customer
         )
         self.url = factories.PaymentProfileFactory.get_url(profile=self.profile)
+        self.customer_url = structure_factories.CustomerFactory.get_url(
+            customer=self.fixture.customer
+        )
 
     @data('owner', 'staff', 'global_support')
     def test_user_with_access_can_retrieve_customer_profile(self, user):
@@ -44,6 +47,44 @@ class ProfileRetrieveTest(test.APITransactionTestCase):
         self.client.force_authenticate(getattr(self.fixture, user))
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    @data('owner', 'staff', 'global_support')
+    def test_user_with_access_can_retrieve_customer_profile_in_organization_endpoint(
+        self, user
+    ):
+        self.client.force_authenticate(getattr(self.fixture, user))
+        response = self.client.get(self.customer_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['payment_profiles']), 1)
+
+    @data('manager', 'admin')
+    def test_user_cannot_retrieve_customer_profile_in_organization_endpoint(self, user):
+        self.client.force_authenticate(getattr(self.fixture, user))
+        response = self.client.get(self.customer_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['payment_profiles'], None)
+
+    @data('staff', 'global_support')
+    def test_user_with_access_can_retrieve_unactive_customer_profile_in_organization_endpoint(
+        self, user
+    ):
+        self.profile.is_active = False
+        self.profile.save()
+        self.client.force_authenticate(getattr(self.fixture, user))
+        response = self.client.get(self.customer_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['payment_profiles']), 1)
+
+    @data('owner',)
+    def test_user_cannot_retrieve_unactive_customer_profile_in_organization_endpoint(
+        self, user
+    ):
+        self.profile.is_active = False
+        self.profile.save()
+        self.client.force_authenticate(getattr(self.fixture, user))
+        response = self.client.get(self.customer_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['payment_profiles'], [])
 
 
 @ddt
