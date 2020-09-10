b93ed30cb4dea2c841ac361fb6eb595c7d2f5407
Ilja Livenson <ilja@opennodecloud.com>
2020-09-10 15:13:58 -0400

    Fix project version replacement to emulate semver

--- docker_push.sh
+++ docker_push.sh
@@ -3,15 +3,17 @@ set -e
 
 if [ -z "$1" ]
 then
-  version='latest'
+  image_version='latest'
+  package_version="0.0.0-$CI_COMMIT_SHORT_SHA"
 else
   # Strip prefix from tag name so that v3.7.5 becomes 3.7.5
-  version=${1#v}
+  image_version=${1#v}
+  package_version=$image_version
 fi
 
 echo "$WALDUR_DOCKER_HUB_PASSWORD" | docker login -u "$WALDUR_DOCKER_HUB_USER" --password-stdin
-sed -i "s/version = \"0.0.0\"/version = \"$version\"/" pyproject.toml
+sed -i "s/version = \"0.0.0\"/version = \"$package_version\"/" pyproject.toml
 
-docker build -t opennode/waldur-mastermind:$version .
-docker push "opennode/waldur-mastermind:$version"
+docker build -t opennode/waldur-mastermind:$image_version .
+docker push "opennode/waldur-mastermind:$image_version"
 docker images
