01c17356bd9eed56979c55ccb55659508d08b218
Victor Mireyev <victor.mireyev@gmail.com>
2020-09-11 12:01:10 +0300

    Fix migration: don't use virtual field scope.

--- src/waldur_openstack/openstack_tenant/migrations/0004_fill_tenant_id.py
+++ src/waldur_openstack/openstack_tenant/migrations/0004_fill_tenant_id.py
@@ -1,17 +1,21 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import migrations
 
 
 def fill_tenant_id(apps, schema_editor):
     ServiceSettings = apps.get_model('structure', 'ServiceSettings')
+    Tenant = apps.get_model('openstack', 'Tenant')
     for service_settings in ServiceSettings.objects.filter(type='OpenStackTenant'):
-        tenant = service_settings.scope
-        if (
-            tenant
-            and tenant.backend_id
-            and not service_settings.options.get('tenant_id')
-        ):
-            service_settings.options['tenant_id'] = tenant.backend_id
-            service_settings.save(update_fields=['options'])
+        if not service_settings.object_id:
+            continue
+        try:
+            tenant = Tenant.objects.get(id=service_settings.object_id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            if tenant.backend_id and not service_settings.options.get('tenant_id'):
+                service_settings.options['tenant_id'] = tenant.backend_id
+                service_settings.save(update_fields=['options'])
 
 
 class Migration(migrations.Migration):
