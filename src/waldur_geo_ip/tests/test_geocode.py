054758af9b66315958985c6641ff1dec08d1313b
Maxim Prokopenko <maxim.prokopenko@gmail.com>
2020-10-22 09:34:31 -0400

    [WAL-3548] Make geodecoding a generic endpoint.

---
+++ src/waldur_geo_ip/tests/test_geocode.py
@@ -0,0 +1,31 @@
+from unittest import mock
+
+from rest_framework import status, test
+from rest_framework.reverse import reverse
+
+from waldur_core.structure.tests import fixtures
+
+
+class OfferingGeoCodeTest(test.APITransactionTestCase):
+    def setUp(self):
+        self.url = reverse('geocode')
+        self.fixture = fixtures.UserFixture()
+
+        mock_patch = mock.patch('waldur_core.core.utils.Nominatim')
+        mock_nominatim = mock_patch.start()
+
+        class Location(object):
+            latitude = 10.0
+            longitude = 10.0
+
+        mock_nominatim().geocode.return_value = Location()
+
+    def tearDown(self):
+        super(OfferingGeoCodeTest, self).tearDown()
+        mock.patch.stopall()
+
+    def test_get_lat_lon_from_address(self):
+        self.client.force_authenticate(self.fixture.staff)
+        response = self.client.get(self.url, {'address': 'Address'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'latitude': 10.0, 'longitude': 10.0})
---
+++ src/waldur_geo_ip/views.py
@@ -0,0 +1,20 @@
+from rest_framework import status, views
+from rest_framework.response import Response
+
+from waldur_core.core.utils import get_lat_lon_from_address
+
+from . import serializers
+
+
+class GeocodeViewSet(views.APIView):
+    def get(self, request):
+        serializer = serializers.GeoCodeSerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        address = serializer.validated_data['address']
+        lat_lon = get_lat_lon_from_address(address)
+        if lat_lon:
+            return Response(
+                {'latitude': lat_lon[0], 'longitude': lat_lon[1]},
+                status=status.HTTP_200_OK,
+            )
+        return Response(None, status=status.HTTP_200_OK)
