50efb6decd3803c52d7c1f13f76a7d3135f573b8
Victor Mireyev <victor.mireyev@gmail.com>
2020-11-13 16:05:16 +0300

    Fix linter.

--- src/waldur_mastermind/marketplace/models.py
+++ src/waldur_mastermind/marketplace/models.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.postgres.fields import JSONField as BetterJSONField
-from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
 from django.db import models
 from django.db.models import Q
