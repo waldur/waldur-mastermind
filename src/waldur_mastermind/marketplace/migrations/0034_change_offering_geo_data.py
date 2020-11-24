85df39bb82907dfec52735be3096c926c6b8bd54
Victor Mireyev <victor.mireyev@gmail.com>
2020-11-24 18:42:44 +0300

    Fix typo in database migration script.

--- src/waldur_mastermind/marketplace/migrations/0034_change_offering_geo_data.py
+++ src/waldur_mastermind/marketplace/migrations/0034_change_offering_geo_data.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 def fill_new_geo_fields(apps, schema_editor):
     Offering = apps.get_model('marketplace', 'Offering')
     for offering in Offering.objects.all():
-        if not offering.geolocations:
+        if offering.geolocations:
             geolocation = offering.geolocations[0]
             offering.latitude = geolocation['latitude']
             offering.longitude = geolocation['longitude']
