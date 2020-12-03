3fee855c763456085d57cd9c2b5c5b4c724f25d6
Victor Mireyev <victor.mireyev@gmail.com>
2020-12-03 18:49:45 +0300

    Include customer owner implicitly if offering and role are specified.

---
+++ src/waldur_mastermind/notifications/tests/test_utils.py
@@ -0,0 +1,60 @@
+from rest_framework import test
+
+from waldur_core.structure.models import CustomerRole, ProjectRole
+from waldur_core.structure.tests import fixtures
+from waldur_mastermind.marketplace.models import Resource
+from waldur_mastermind.marketplace.tests import factories as marketplace_factories
+
+from ..utils import get_users_for_query
+
+
+class UsersFilterTest(test.APITransactionTestCase):
+    def setUp(self):
+        self.fixture = fixtures.ServiceFixture()
+        self.project = self.fixture.project
+        self.plan = marketplace_factories.PlanFactory()
+        self.offering = self.plan.offering
+        self.resource = Resource.objects.create(
+            project=self.project, offering=self.offering, plan=self.plan,
+        )
+
+    def test_offering_and_customer_roles_are_specified(self):
+        owner = self.fixture.owner
+        manager = self.fixture.manager
+        users = get_users_for_query(
+            {'customer_roles': [CustomerRole.OWNER], 'offerings': [self.offering]}
+        )
+        self.assertIn(owner, users)
+        self.assertNotIn(manager, users)
+
+    def test_offering_and_project_roles_are_specified(self):
+        manager = self.fixture.manager
+        admin = self.fixture.admin
+        users = get_users_for_query(
+            {'project_roles': [ProjectRole.MANAGER], 'offerings': [self.offering]}
+        )
+        self.assertIn(manager, users)
+        self.assertNotIn(admin, users)
+
+    def test_project_is_specified_explicitly(self):
+        owner = self.fixture.owner
+        manager = self.fixture.manager
+        admin = self.fixture.admin
+        users = get_users_for_query({'projects': [self.fixture.project],})
+        self.assertIn(manager, users)
+        self.assertIn(admin, users)
+        self.assertNotIn(owner, users)
+
+    def test_customer_is_specified_explicitly(self):
+        owner = self.fixture.owner
+        manager = self.fixture.manager
+        admin = self.fixture.admin
+        users = get_users_for_query(
+            {
+                'customers': [self.fixture.customer],
+                'customer_roles': [CustomerRole.OWNER],
+            }
+        )
+        self.assertNotIn(manager, users)
+        self.assertNotIn(admin, users)
+        self.assertIn(owner, users)
