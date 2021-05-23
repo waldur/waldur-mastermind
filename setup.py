29ce34ead19888b560a5c1ecfc34c4ae8d4d4251
Ilja Livenson <ilja@opennodecloud.com>
2021-05-23 18:16:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ tests_requires = [
 
 setup(
     name='ansible-waldur-module',
-    version='1.1.0',
+    version='1.1.1',
     author='OpenNode Team',
     author_email='info@opennodecloud.com',
     url='https://waldur.com',
