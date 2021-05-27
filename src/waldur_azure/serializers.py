cbce00a46d4ab4ba778f67f287b65ba810c8ba84
Victor Mireyev <victor@opennodecloud.com>
2021-05-28 02:04:58 +0300

    Drop storage account creation.

--- src/waldur_azure/serializers.py
+++ src/waldur_azure/serializers.py
@@ -4,7 +4,7 @@ from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
-from waldur_azure.utils import generate_password, generate_username, hash_string
+from waldur_azure.utils import generate_password, generate_username
 from waldur_core.core.models import SshPublicKey
 from waldur_core.structure import serializers as structure_serializers
 from waldur_core.structure.models import ServiceSettings
@@ -195,7 +195,6 @@ class VirtualMachineSerializer(
         location = validated_data.pop('location')
 
         resource_group_name = f'group-{uuid.uuid4().hex[:4]}-{vm_name}'
-        storage_account_name = 'storage{}'.format(hash_string(vm_name.lower(), 14))
         network_name = 'net{}'.format(vm_name)
         subnet_name = 'subnet{}'.format(vm_name)
         nic_name = 'nic{}'.format(vm_name)
@@ -208,13 +207,6 @@ class VirtualMachineSerializer(
             project=project,
             name=resource_group_name,
             location=location,
-        )
-
-        models.StorageAccount.objects.create(
-            service_settings=service_settings,
-            project=project,
-            name=storage_account_name,
-            resource_group=resource_group,
         )
 
         network = models.Network.objects.create(
