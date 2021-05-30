a9c9e8572cc936492515aae4f603fec23260619f
Ilja Livenson <ilja@opennodecloud.com>
2021-05-30 15:50:27 +0300

    Move PID to metadata

--- src/waldur_pid/extension.py
+++ src/waldur_pid/extension.py
@@ -4,18 +4,6 @@ from waldur_core.core import WaldurExtension
 
 
 class PIDExtension(WaldurExtension):
-    class Settings:
-        WALDUR_PID = {
-            'DATACITE': {
-                'REPOSITORY_ID': '',
-                'PASSWORD': '',
-                'PREFIX': '',
-                'API_URL': 'https://example.com',
-                'PUBLISHER': 'Waldur',
-                'COLLECTION_DOI': '',
-            }
-        }
-
     @staticmethod
     def django_app():
         return 'waldur_pid'
