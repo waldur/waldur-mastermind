f41e078f62072346bc938d0a88232b83939a394d
Sergei Zaiaev <serjzv@gmail.com>
2021-09-14 18:09:38 +0000

    [WAL-4304] Implement command to generate event guide to waldur-docs

---
+++ src/waldur_core/core/management/commands/print_events.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from waldur_core.logging.loggers import event_logger
+
+BLANK_LINE = '\n\n'
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print("# Events", end=BLANK_LINE)
+        groups = sorted([(k, v) for k, v in event_logger.get_all_groups().items()])
+        for event_group, events in groups:
+            print(f"## {str(event_group).capitalize()}", end=BLANK_LINE)
+            for event in sorted(events):
+                print(f"- {event}")
+            print()
