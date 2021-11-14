9a35fc22890e24345e34b729649c1cb075917b46
ilja <ilja@opennodecloud.com>
2021-11-14 19:09:08 +0200

    Add cleanup of API settings cache on DB init

---
+++ src/waldur_core/core/management/commands/clean_settings_cache.py
@@ -0,0 +1,12 @@
+from django.core.cache import cache
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Clean API configuration settings cache."
+
+    def handle(self, *args, **options):
+        cache.delete('API_CONFIGURATION')
+        self.stdout.write(
+            self.style.SUCCESS('API_CONFIGURATION key has been cleaned from cache.')
+        )
