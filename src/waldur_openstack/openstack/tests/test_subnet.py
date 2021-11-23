7c6f8f57d6258ffc5f8c606b1051b2cb14bf267a
Victor Mireyev <victor.mireyev@gmail.com>
2021-11-23 13:17:40 +0200

    Fix CIDR overlap validation: use tenant scope instead of service [WAL-4488]

--- src/waldur_openstack/openstack/tests/test_subnet.py
+++ src/waldur_openstack/openstack/tests/test_subnet.py
@@ -10,17 +10,6 @@ class BaseSubNetTest(test.APITransactionTestCase):
         self.fixture = fixtures.OpenStackFixture()
 
 
-class SubNetCreateActionTest(BaseSubNetTest):
-    def setUp(self):
-        super(SubNetCreateActionTest, self).setUp()
-        self.client.force_authenticate(user=self.fixture.user)
-
-    def test_subnet_create_action_is_not_allowed(self):
-        url = factories.SubNetFactory.get_list_url()
-        response = self.client.post(url)
-        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
 @mock.patch('waldur_openstack.openstack.executors.SubNetDeleteExecutor.execute')
 class SubNetDeleteActionTest(BaseSubNetTest):
     def setUp(self):
