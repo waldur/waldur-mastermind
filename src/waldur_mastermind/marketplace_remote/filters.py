33866a49226c680c0b0b9a568f47f5dc83c3ca39
Victor Mireyev <victor.mireyev@gmail.com>
2021-12-20 15:17:47 +0200

    Allow to filter project update request by state [WAL-4556]

--- src/waldur_mastermind/marketplace_remote/filters.py
+++ src/waldur_mastermind/marketplace_remote/filters.py
@@ -1,5 +1,7 @@
 import django_filters
 
+from waldur_core.core import filters as core_filters
+
 from . import models
 
 
@@ -10,6 +12,7 @@ class ProjectUpdateRequestFilter(django_filters.FilterSet):
     offering_customer_uuid = django_filters.UUIDFilter(
         field_name='offering__customer__uuid'
     )
+    state = core_filters.ReviewStateFilter()
 
     class Meta:
         model = models.ProjectUpdateRequest
