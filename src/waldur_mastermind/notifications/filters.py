73a89fdd5262c3b363c1721cdf141d4747e1a2d6
Victor Mireyev <victor.mireyev@gmail.com>
2022-05-06 17:24:49 +0300

    Allow to filter notification by subject.

--- src/waldur_mastermind/notifications/filters.py
+++ src/waldur_mastermind/notifications/filters.py
@@ -2,8 +2,14 @@ import django_filters
 
 from waldur_core.core import filters as core_filters
 
+from . import models
+
 
 class NotificationFilterSet(django_filters.FilterSet):
+    class Meta:
+        model = models.Notification
+        fields = ('subject',)
+
     o = core_filters.ExtendedOrderingFilter(
         fields=(
             ('created', 'created'),
