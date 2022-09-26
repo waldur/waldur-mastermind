f0bab26231b69e09a3e98ae5e4980b89cc7f48d6
Viktor Mirieiev <victor.mireyev@gmail.com>
2022-09-26 11:25:40 +0300

    Bump poetry

--- docker_build.sh
+++ docker_build.sh
@@ -4,7 +4,7 @@ set -e
 python3 -m pip install --upgrade pip
 # Upgrade setuptools to the latest
 python3 -m pip install --upgrade setuptools==65.4.0
-python3 -m pip install poetry==1.1.11
+python3 -m pip install poetry==1.2.1
 poetry config experimental.new-installer false
 poetry config virtualenvs.create false
 
