fe2b31ebe39436c05f8eeebc2a87539d00500e43
Adil Shirinov <adil.shirinov.99@gmail.com>
2022-10-07 13:06:38 +0000

    Disable feature toggling for group invitations[WAL-5131]

--- src/waldur_core/core/features.py
+++ src/waldur_core/core/features.py
@@ -214,10 +214,6 @@ class InvitationsSection(FeatureSection):
         'Render "Show user details" button in invitation creation form.'
     )
 
-    show_group_invitations = Feature(
-        'Render elements supporting group invitation management.'
-    )
-
 
 class InvoiceSection(FeatureSection):
     class Meta:
