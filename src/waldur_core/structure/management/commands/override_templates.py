65dfa348baa3b1185124a971de2bf498c80b6b56
Adil Shirinov <adil.shirinov.99@gmail.com>
2022-11-03 15:32:40 +0000

    Management command to override templates

---
+++ src/waldur_core/structure/management/commands/override_templates.py
@@ -0,0 +1,35 @@
+import yaml
+from dbtemplates.models import Template
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Override templates"
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+        parser.add_argument(
+            'templates_file',
+            help='Specifies location of templates file.',
+        )
+        parser.add_argument(
+            '-c',
+            '--clean',
+            dest='clean',
+            default=False,
+            help='This flag means total synchronization with the template file you pass.',
+        )
+
+    def handle(self, *args, **options):
+
+        with open(options['templates_file'], 'r') as templates_file:
+            templates = yaml.safe_load(templates_file)
+
+        if options['clean']:
+            all_templates = Template.objects.all()
+            for template in all_templates:
+                if template.name not in templates:
+                    Template.objects.get(name=template.name).delete()
+
+        for path, content in templates.items():
+            Template.objects.filter(name=path).update(content=content)
