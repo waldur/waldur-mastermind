637e4712d149e5b932f9aef64b4f0caf1b8ddf3d
Sergei Zaiaev <serjzv@gmail.com>
2022-12-08 11:58:35 +0000

    [WAL-5043] Update TOS and PP loading

--- src/waldur_core/structure/management/commands/load_user_agreements.py
+++ src/waldur_core/structure/management/commands/load_user_agreements.py
@@ -3,19 +3,31 @@ from django.core.management.base import BaseCommand
 from waldur_core.structure.models import UserAgreement
 
 
-def create_user_agreement(filepath, agreement_type):
-    try:
-        with open(filepath, 'r') as agreement_file:
-            content = agreement_file.read()
-        UserAgreement.objects.update_or_create(
-            agreement_type=agreement_type, defaults={'content': content}
-        )
-    except Exception as e:
-        return e
-
-
 class Command(BaseCommand):
     help = 'Imports privacy policy and terms of service into DB'
+
+    def create_user_agreement(self, filepath, agreement_type, force=False):
+        try:
+            user_agreement_count = UserAgreement.objects.filter(
+                agreement_type=agreement_type
+            ).count()
+            if not force and user_agreement_count > 0:
+                self.stdout.write(
+                    self.style.NOTICE(
+                        'The %s agreement already exists, skipping loading'
+                        % agreement_type,
+                    )
+                )
+                return
+
+            with open(filepath, 'r') as agreement_file:
+                content = agreement_file.read()
+
+            UserAgreement.objects.update_or_create(
+                agreement_type=agreement_type, defaults={'content': content}
+            )
+        except Exception as e:
+            return e
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -32,10 +44,18 @@ class Command(BaseCommand):
             help='Path to a Privacy policy file',
             required=False,
         )
+        parser.add_argument(
+            '-f',
+            '--force',
+            dest='force',
+            default=False,
+            help='This flag means force loading agreements even if they are already defined in DB.',
+        )
 
     def handle(self, *args, **options):
         tos_path = options.get('tos')
         pp_path = options.get('pp')
+        force = options.get('force')
 
         if not tos_path and not pp_path:
             self.stdout.write(
@@ -47,14 +67,18 @@ class Command(BaseCommand):
 
         if tos_path:
             try:
-                create_user_agreement(tos_path, UserAgreement.UserAgreements.TOS)
+                self.create_user_agreement(
+                    tos_path, UserAgreement.UserAgreements.TOS, force
+                )
             except Exception:
                 self.stdout.write(self.style.ERROR("Couldn't create Terms of Service"))
                 return
 
         if pp_path:
             try:
-                create_user_agreement(pp_path, UserAgreement.UserAgreements.PP)
+                self.create_user_agreement(
+                    pp_path, UserAgreement.UserAgreements.PP, force
+                )
             except Exception:
                 self.stdout.write(self.style.ERROR("Couldn't create Privacy policy"))
                 return
