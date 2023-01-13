17b407b505668dbb0ede3d096fe234036cdb354c
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-01-13 16:26:49 +0200

    Allow to filter campaing by service provider UUID.

--- src/waldur_mastermind/promotions/filters.py
+++ src/waldur_mastermind/promotions/filters.py
@@ -2,15 +2,23 @@ import django_filters
 
 from waldur_core.core import filters as core_filters
 
+from . import models
+
 
 class CampaignFilter(django_filters.FilterSet):
+    class Meta:
+        model = models.Campaign
+        fields = []
+
     offering = core_filters.URLFilter(
         view_name='marketplace-provider-offering-detail',
         field_name='offering__uuid',
         label='Offering',
     )
     offering_uuid = django_filters.UUIDFilter(field_name='offering__uuid')
-    is_active = django_filters.BooleanFilter(field_name='is_active')
+    service_provider_uuid = django_filters.UUIDFilter(
+        field_name='service_provider__uuid'
+    )
 
     o = django_filters.OrderingFilter(
         fields=(
