56dbf90852298167fc281e74ed55a5897e10b1c1
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-02-08 12:39:55 +0200

    Allow to filter order item by multiple offering types at once [WAL-5494]

--- src/waldur_mastermind/marketplace/filters.py
+++ src/waldur_mastermind/marketplace/filters.py
@@ -296,7 +296,7 @@ class OrderFilter(django_filters.FilterSet):
 
 class OrderItemFilter(OfferingFilterMixin, django_filters.FilterSet):
     project_uuid = django_filters.UUIDFilter(field_name='order__project__uuid')
-    offering_type = django_filters.CharFilter(
+    offering_type = core_filters.LooseMultipleChoiceFilter(
         field_name='offering__type', lookup_expr='exact'
     )
     category_uuid = django_filters.UUIDFilter(field_name='offering__category__uuid')
