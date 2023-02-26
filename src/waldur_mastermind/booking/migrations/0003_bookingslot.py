26ef30f79cc813da7778a7dad62c5b91e5358793
Ilja Livenson <livenson@users.noreply.github.com>
2023-02-26 11:49:34 +0200

    Fix formatting

--- src/waldur_mastermind/booking/migrations/0003_bookingslot.py
+++ src/waldur_mastermind/booking/migrations/0003_bookingslot.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('marketplace', '0079_componentusage_modified_by'),
         ('booking', '0002_busyslot'),
