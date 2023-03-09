de85315a0d0b6c515719950288cf3e04b452ae61
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-03-09 14:45:59 +0200

    Fix data migration

--- src/waldur_mastermind/marketplace_slurm_remote/migrations/0001_create_offering_users.py
+++ src/waldur_mastermind/marketplace_slurm_remote/migrations/0001_create_offering_users.py
@@ -1,13 +1,20 @@
 from django.db import migrations
 
 
+class States:
+    DRAFT = 1
+    ACTIVE = 2
+    PAUSED = 3
+    ARCHIVED = 4
+
+
 def create_offering_users_for_existing_users(apps, schema_editor):
     from waldur_mastermind.marketplace_slurm_remote import utils
 
     Offering = apps.get_model('marketplace', 'Offering')
     offerings = Offering.objects.filter(
         type='Marketplace.Slurm',
-        state__in=[Offering.States.ACTIVE, Offering.States.PAUSED],
+        state__in=[States.ACTIVE, States.PAUSED],
     )
 
     for offering in offerings:
