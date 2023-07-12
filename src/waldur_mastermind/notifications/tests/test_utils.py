10e1d621f196918e410fea49f420532d12b4c47a
Maxim Prokopenko <maxim.prokopenko@gmail.com>
2023-07-12 14:31:56 +0000

    [WAL-5792] Sent emails with using bcc.

--- src/waldur_mastermind/notifications/tests/test_utils.py
+++ src/waldur_mastermind/notifications/tests/test_utils.py
@@ -1,8 +1,12 @@
+from unittest import mock
+
 from rest_framework import test
 
 from waldur_core.structure.tests import fixtures
 from waldur_mastermind.marketplace.models import Resource
 from waldur_mastermind.marketplace.tests import factories as marketplace_factories
+from waldur_mastermind.notifications import tasks as notifications_tasks
+from waldur_mastermind.notifications.tests import factories as notifications_factories
 
 from ..utils import get_users_for_query
 
@@ -41,3 +45,23 @@ class UsersFilterTest(test.APITransactionTestCase):
         )
         self.assertIn(owner, users)
         self.assertIn(manager, users)
+
+
+class TaskTest(test.APITransactionTestCase):
+    def setUp(self):
+        self.emails_1 = ['email_%s@gmail.com' % i for i in range(1, 51)]
+        self.emails_2 = ['email_%s@gmail.com' % i for i in range(51, 101)]
+        self.emails_3 = ['email_%s@gmail.com' % i for i in range(101, 110)]
+        self.broadcast = notifications_factories.BroadcastMessageFactory(
+            query='', emails=self.emails_1 + self.emails_2 + self.emails_3
+        )
+
+    @mock.patch('waldur_mastermind.notifications.tasks.send_mail')
+    def test_send_broadcast_message_email(self, send_mail_mock):
+        notifications_tasks.send_broadcast_message_email(self.broadcast.uuid.hex)
+        self.assertEqual(send_mail_mock.call_count, 3)
+        self.assertEqual(send_mail_mock.call_args_list[0].kwargs['bcc'], self.emails_1)
+
+        self.assertEqual(send_mail_mock.call_args_list[1].kwargs['bcc'], self.emails_2)
+
+        self.assertEqual(send_mail_mock.call_args_list[2].kwargs['bcc'], self.emails_3)
