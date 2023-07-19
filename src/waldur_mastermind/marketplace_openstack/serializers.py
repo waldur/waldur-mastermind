032bc18e517c67e159e29aaa3cd8546de374cca9
Maxim Prokopenko <maximprokopenko@gmail.com>
2023-07-19 17:45:05 +0200

    [WAL-5930] Add marketplace_resource_uuid to nested serializer of volumes in instance.

--- src/waldur_mastermind/marketplace_openstack/serializers.py
+++ src/waldur_mastermind/marketplace_openstack/serializers.py
@@ -1,8 +1,13 @@
 from django.db import transaction
 from rest_framework import serializers
 
+from waldur_core.core import signals as core_signals
+from waldur_mastermind.marketplace import models as marketplace_models
 from waldur_mastermind.marketplace_openstack.utils import _apply_quotas
 from waldur_openstack.openstack import serializers as openstack_serializers
+from waldur_openstack.openstack_tenant import (
+    serializers as openstack_tenant_serializers,
+)
 
 
 class MarketplaceTenantCreateSerializer(openstack_serializers.TenantSerializer):
@@ -25,3 +30,22 @@ class MarketplaceTenantCreateSerializer(openstack_serializers.TenantSerializer):
 
     def _validate_service_settings(self, service_settings, project):
         pass
+
+
+def get_marketplace_resource_uuid(serializer, volume):
+    try:
+        resource = marketplace_models.Resource.objects.filter(scope=volume).get()
+        return resource.uuid.hex
+    except marketplace_models.Resource.DoesNotExist:
+        return
+
+
+def add_marketplace_resource_uuid(sender, fields, **kwargs):
+    fields['marketplace_resource_uuid'] = serializers.SerializerMethodField()
+    setattr(sender, 'get_marketplace_resource_uuid', get_marketplace_resource_uuid)
+
+
+core_signals.pre_serializer_fields.connect(
+    sender=openstack_tenant_serializers.NestedVolumeSerializer,
+    receiver=add_marketplace_resource_uuid,
+)
