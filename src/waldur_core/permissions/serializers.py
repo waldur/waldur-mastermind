8106b0c006e6ec392c5fadd00753a7ad498ba882
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-08-28 21:24:31 +0000

    Add support for user role management [WAL-6043]

--- src/waldur_core/permissions/serializers.py
+++ src/waldur_core/permissions/serializers.py
@@ -1,16 +1,55 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from waldur_core.permissions.enums import PermissionEnum
 
 from . import models
 
 
-class RoleSerializer(serializers.ModelSerializer):
+class RoleDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Role
-        fields = ('name', 'description', 'permissions')
+        fields = ('uuid', 'name', 'description', 'permissions')
 
     permissions = serializers.SerializerMethodField()
 
     def get_permissions(self, role):
-        return models.RolePermission.objects.filter(role=role).values_list(
-            'permission', flat=True
+        return list(
+            models.RolePermission.objects.filter(role=role).values_list(
+                'permission', flat=True
+            )
         )
+
+
+class RoleModifySerializer(RoleDetailsSerializer):
+    permissions = serializers.JSONField()
+
+    def validate_permissions(self, permissions):
+        invalid = set(permissions) - set(perm.value for perm in PermissionEnum)
+        if invalid:
+            raise ValidationError(f'Invalid permissions {",".join(invalid)}')
+        return permissions
+
+    def create(self, validated_data):
+        permissions = validated_data.pop('permissions')
+        role = super().create(validated_data)
+        for permission in permissions:
+            models.RolePermission.objects.create(role=role, permission=permission)
+        return role
+
+    def update(self, instance, validated_data):
+        current_permissions = set(
+            models.RolePermission.objects.filter(role=instance).values_list(
+                'permission', flat=True
+            )
+        )
+        new_permissions = set(validated_data.pop('permissions'))
+        role = super().update(instance, validated_data)
+        models.RolePermission.objects.filter(
+            role=role, permission__in=current_permissions - new_permissions
+        ).delete()
+
+        for permission in new_permissions - current_permissions:
+            models.RolePermission.objects.create(role=role, permission=permission)
+
+        return role
--- src/waldur_core/permissions/views.py
+++ src/waldur_core/permissions/views.py
@@ -1,9 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.response import Response
+
+from waldur_core.core.permissions import IsAdminOrReadOnly
 
 from . import models, serializers
 
 
-class RoleViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = ()
+class RoleViewSet(viewsets.ModelViewSet):
     queryset = models.Role.objects.all()
-    serializer_class = serializers.RoleSerializer
+    serializer_class = serializers.RoleDetailsSerializer
+    lookup_field = 'uuid'
+    permission_classes = [IsAdminOrReadOnly]
+
+    def create(self, request):
+        serializer = serializers.RoleModifySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        role = serializer.save()
+        serializer = serializers.RoleDetailsSerializer(instance=role)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, **kwargs):
+        instance = self.get_object()
+        serializer = serializers.RoleModifySerializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        role = serializer.save()
+        serializer = serializers.RoleDetailsSerializer(instance=role)
+        return Response(serializer.data, status=status.HTTP_200_OK)
