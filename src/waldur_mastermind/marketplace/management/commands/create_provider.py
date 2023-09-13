b21121972b29459d09470cd1cde4fbf64cde8d4d
Emil <ojagverdiyev13@gmail.com>
2023-09-13 12:03:30 +0300

    useless functions removed typo fixed in help text

--- src/waldur_mastermind/marketplace/management/commands/create_provider.py
+++ src/waldur_mastermind/marketplace/management/commands/create_provider.py
@@ -5,31 +5,19 @@ from waldur_core.structure.models import Customer
 from waldur_mastermind.marketplace.models import ServiceProvider
 
 
-def create_customer(name):
-    customer, created = Customer.objects.get_or_create(name=name)
-    if created:
-        return customer
-    else:
-        return None
-
-
-def create_provider(customer):
-    ServiceProvider.objects.create(customer=customer)
-
-
 class Command(BaseCommand):
     help = "Create service provider"
 
     def add_arguments(self, parser):
         parser.add_argument('-n', type=str, help='Customer name')
-        parser.add_argument('-c', nargs='+', type=str, help='Category List of categories to load')
+        parser.add_argument('-c', nargs='+', type=str, help='List of categories to load')
 
     def handle(self, *args, **options):
         name = options['n']
-        customer_instance = create_customer(name)
-        if customer_instance is not None:
+        customer, created = Customer.objects.get_or_create(name=name)
+        if created:
             call_command('load_categories', options['c'])
-            create_provider(customer_instance)
+            ServiceProvider.objects.create(customer=customer)
             self.stdout.write(self.style.SUCCESS('Service Provider created'))
         else:
             self.stdout.write(self.style.ERROR(f'Customer {name} already exists'))
