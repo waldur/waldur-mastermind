465e0414304623d8f55ebe25609441f6df5f7d04
Maxim Prokopenko <maximprokopenko@gmail.com>
2023-09-28 12:20:38 +0200

    [WAL-6112] Fix duplicate results in project search.

--- src/waldur_core/structure/filters.py
+++ src/waldur_core/structure/filters.py
@@ -272,18 +272,19 @@ class ProjectFilter(NameFilterSet):
 
     def filter_query(self, queryset, name, value):
         if is_uuid_like(value):
-            return queryset.filter(
+            queryset = queryset.filter(
                 Q(uuid=value)
                 | Q(resource__backend_id=value)
                 | Q(resource__effective_id=value)
             )
         else:
-            return queryset.filter(
+            queryset = queryset.filter(
                 Q(resource__name=value)
                 | Q(name__icontains=value)
                 | Q(resource__backend_id=value)
                 | Q(resource__effective_id=value)
             )
+        return queryset.distinct()
 
 
 class CustomerUserFilter(BaseFilterBackend):
