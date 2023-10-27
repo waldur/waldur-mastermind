90bec2b321561d54858720affa033a3e1bd69fd3
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-10-28 02:03:55 +0300

    Drop jinja2

--- src/waldur_core/permissions/print_permission_enums.py
+++ src/waldur_core/permissions/print_permission_enums.py
@@ -1,27 +1,22 @@
-from jinja2 import Template
+import enums
 
-from . import enums
 
-template = Template(
-    """
+def format_enum(enum):
+    return '\n'.join(f'  {key}: "{value}",' for key, value in enum._member_map_.items())
+
+
+print(
+    f"""
 /* eslint-disable */
 // WARNING: This file is auto-generated from src/waldur_core/permissions/enums.py
 // Do not edit it manually. All manual changes would be overridden.
 
-export const RoleEnum = {
-{% for key, value in roles %}
-  {{ key }}: '{{ value.value }}',
-{% endfor %}};
+export const RoleEnum = {{
+{format_enum(enums.RoleEnum)}
+}};
 
-export const PermissionEnum = {
-{% for key, value in permissions %}
-  {{ key }}: '{{ value.value }}',
-{% endfor %}};
+export const PermissionEnum = {{
+{format_enum(enums.PermissionEnum)}
+}};
 """
 )
-
-context = {
-    'roles': enums.RoleEnum._member_map_.items(),
-    'permissions': enums.PermissionEnum._member_map_.items(),
-}
-print(template.render(context).replace('\n\n', '\n'))
