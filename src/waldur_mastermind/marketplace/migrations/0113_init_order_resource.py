449a1182c87c050e172cdfa6ac0f0ce58438cf7d
Viktor Mirieiev <victor.mireyev@gmail.com>
2023-12-22 11:22:31 +0200

    Truncate resource name to 150 chars.

--- src/waldur_mastermind/marketplace/migrations/0113_init_order_resource.py
+++ src/waldur_mastermind/marketplace/migrations/0113_init_order_resource.py
@@ -5,13 +5,14 @@ def init_order_resource(apps, schema_editor):
     Order = apps.get_model('marketplace', 'Order')
     Resource = apps.get_model('marketplace', 'Resource')
     for order in Order.objects.filter(resource__isnull=True):
+        name = order.attributes.get('name') or 'resource'
         resource = Resource.objects.create(
             project=order.project,
             offering=order.offering,
             plan=order.plan,
             limits=order.limits,
             attributes=order.attributes,
-            name=order.attributes.get('name') or 'resource',
+            name=name[:150],
         )
         order.resource = resource
         order.save(update_fields=['resource'])
