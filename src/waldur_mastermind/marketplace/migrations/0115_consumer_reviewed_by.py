a33c4cf2d24de01fd9070564840b4f0ce9a4f7f1
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-01-04 13:59:51 +0200

    Init consumer_reviewed_by for order if it is autoapproved [WAL-6404]

---
+++ src/waldur_mastermind/marketplace/migrations/0115_consumer_reviewed_by.py
@@ -0,0 +1,33 @@
+from django.db import migrations, models
+
+
+class States:
+    PENDING_CONSUMER = 1
+    PENDING_PROVIDER = 7
+    EXECUTING = 2
+    DONE = 3
+    ERRED = 4
+    CANCELED = 5
+    REJECTED = 6
+
+
+def init_consumer_reviewed_by(apps, schema_editor):
+    Order = apps.get_model('marketplace', 'Order')
+    Order.objects.filter(
+        consumer_reviewed_by__isnull=True,
+        consumer_reviewed_at__isnull=True,
+        state__in=(States.PENDING_PROVIDER, States.EXECUTING, States.DONE),
+    ).update(
+        consumer_reviewed_by=models.F('created_by'),
+        consumer_reviewed_at=models.F('created'),
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('marketplace', '0114_alter_order_resource'),
+    ]
+
+    operations = [
+        migrations.RunPython(init_consumer_reviewed_by),
+    ]
