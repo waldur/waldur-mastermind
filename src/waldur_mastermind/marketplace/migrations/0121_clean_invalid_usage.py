60df081ab70398350120f3ec2c90c51eb70fd94b
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-01-11 13:06:05 +0000

    Remove usage created before resource

--- src/waldur_mastermind/marketplace/migrations/0121_clean_invalid_usage.py
+++ src/waldur_mastermind/marketplace/migrations/0121_clean_invalid_usage.py
@@ -1,4 +1,6 @@
 from django.db import migrations
+from django.db.models import F
+
 
 PLUGIN_NAME = "Waldur.RemoteOffering"
 
@@ -7,6 +9,9 @@ def clean_componentusage(apps, schema_editor):
     ComponentUsage = apps.get_model("marketplace", "ComponentUsage")
     ComponentUsage.objects.filter(
         resource__offering__type=PLUGIN_NAME, resource__backend_id=""
+    ).delete()
+    ComponentUsage.objects.filter(
+        resource__offering__type=PLUGIN_NAME, date__lt=F('resource__created')
     ).delete()
 
 
