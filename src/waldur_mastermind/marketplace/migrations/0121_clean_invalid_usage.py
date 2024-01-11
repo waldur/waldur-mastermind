4adfd01fac79a25aca938422391592e950962790
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-01-11 12:05:05 +0200

    Cleanup invalid component usage for remote offerings [WAL-6426]

---
+++ src/waldur_mastermind/marketplace/migrations/0121_clean_invalid_usage.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+PLUGIN_NAME = "Waldur.RemoteOffering"
+
+
+def clean_componentusage(apps, schema_editor):
+    ComponentUsage = apps.get_model("marketplace", "ComponentUsage")
+    ComponentUsage.objects.filter(
+        resource__offering__type=PLUGIN_NAME, resource__backend_id=""
+    ).delete()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("marketplace", "0120_componentusage_unique_together"),
+    ]
+
+    operations = [
+        migrations.RunPython(clean_componentusage),
+    ]
