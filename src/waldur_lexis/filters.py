03674547273a329b764d4b7f6947fb4a830363be
Adil Shirinov <dev.adil.shirinov@gmail.com>
2024-02-06 12:38:39 +0000

    [WAL-6460] Improvements to lexis link filters

--- src/waldur_lexis/filters.py
+++ src/waldur_lexis/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from . import models
 
@@ -14,7 +15,14 @@ class LexisLinkFilter(django_filters.FilterSet):
     customer_uuid = django_filters.UUIDFilter(
         field_name="robot_account__resource__customer__uuid"
     )
+    query = django_filters.CharFilter(method="filter_query")
 
     class Meta:
         model = models.LexisLink
         fields = ("uuid", "resource_uuid", "project_uuid", "customer_uuid")
+
+    def filter_query(self, queryset, name, value):
+        return queryset.filter(
+            Q(robot_account__username__icontains=value)
+            | Q(robot_account__type__icontains=value)
+        )
