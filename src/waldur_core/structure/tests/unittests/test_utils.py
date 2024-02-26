82d9c1342f348489c1ef4e582a5ff247cb07423d
Maxim Prokopenko <maximprokopenko@gmail.com>
2024-02-26 16:31:31 +0100

    [WAL-6595] Fix noise in log if directly_connected_ips field hasn't been changed.

--- src/waldur_core/structure/tests/unittests/test_utils.py
+++ src/waldur_core/structure/tests/unittests/test_utils.py
@@ -5,11 +5,18 @@ from waldur_core.structure.utils import update_pulled_fields
 
 
 class InstanceMock:
-    def __init__(self, name="Virtual machine", runtime_state="OK", error_message=""):
+    def __init__(
+        self,
+        name="Virtual machine",
+        runtime_state="OK",
+        error_message="",
+        directly_connected_ips="",
+    ):
         self.pk = 1
         self.name = name
         self.runtime_state = runtime_state
         self.error_message = error_message
+        self.directly_connected_ips = directly_connected_ips
         self.save = mock.Mock()
 
 
@@ -35,4 +42,23 @@ class UpdatePulledFieldsTest(unittest.TestCase):
         vm1 = InstanceMock()
         vm2 = InstanceMock(error_message="Server does not respond.")
         update_pulled_fields(vm1, vm2, ("name",))
+        self.assertEqual(vm1.save.call_count, 1)
+
+    def test_comma_separated_strings_field(self):
+        vm1 = InstanceMock(
+            directly_connected_ips="192.168.42.69,171.22.247.92,172.16.202.63"
+        )
+        vm2 = InstanceMock(
+            directly_connected_ips="172.16.202.63,192.168.42.69,171.22.247.92"
+        )
+        update_pulled_fields(vm1, vm2, ("directly_connected_ips",))
+        self.assertEqual(vm1.save.call_count, 0)
+
+        vm1 = InstanceMock(
+            directly_connected_ips="292.168.42.69,171.22.247.92,172.16.202.63"
+        )
+        vm2 = InstanceMock(
+            directly_connected_ips="172.16.202.63,192.168.42.69,171.22.247.92"
+        )
+        update_pulled_fields(vm1, vm2, ("directly_connected_ips",))
         self.assertEqual(vm1.save.call_count, 1)
