0b234479301786e3f764f00e3ff1951cd77c8260
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-04-05 17:53:31 +0300

    Introduce feature enum

--- src/waldur_core/core/management/commands/print_features_enums.py
+++ src/waldur_core/core/management/commands/print_features_enums.py
@@ -9,10 +9,18 @@ class Command(BaseCommand):
             "// WARNING: This file is auto-generated from src/waldur_core/core/management/commands/print_features_enums.py"
         )
         print("// Do not edit it manually. All manual changes would be overridden.")
+        sections = []
         for section in sorted(FEATURES, key=lambda section: section["key"]):
             section_key = section["key"]
-            print(f"\nexport enum {section_key.capitalize()}Features {{")
+            title = section_key.capitalize() + "Features"
+            sections.append(title)
+            print(f"\nexport enum {title} {{")
 
             for feature in sorted(section["items"], key=lambda section: section["key"]):
                 print(f'  {feature["key"]} = \'{section_key}.{feature["key"]}\',')
             print("}")
+        print()
+        print("export type FeatureEnum =")
+        for section in sections:
+            print(f"  | {section}")
+        print(";")
