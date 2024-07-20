bd6426a06236ce7fb0b49182a4f2ce2e696a4e07
Maxim Prokopenko <maxim.prokopenko@gmail.com>
2024-07-20 15:32:29 +0000

    [WAL-7066] Drop deprecated competence field from user model.

--- src/waldur_core/core/admin.py
+++ src/waldur_core/core/admin.py
@@ -145,15 +145,6 @@ class UserChangeForm(auth_forms.UserChangeForm):
         model = get_user_model()
         exclude = ("details",)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        competences = [
-            (key, key) for key in settings.WALDUR_CORE.get("USER_COMPETENCE_LIST", [])
-        ]
-        self.fields["competence"] = OptionalChoiceField(
-            choices=competences, required=False
-        )
-
     def clean_civil_number(self):
         # Empty string should be converted to None.
         # Otherwise uniqueness constraint is violated.
@@ -245,7 +236,6 @@ class UserAdmin(NativeNameAdminMixin, auth_admin.UserAdmin, VersionAdmin):
                     "native_name",
                     "email",
                     "preferred_language",
-                    "competence",
                     "phone_number",
                 )
             },
