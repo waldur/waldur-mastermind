2001d93846a4dc0d32ea9e43f239fad3ccda9ed6
Maxim Prokopenko <maxim.prokopenko@gmail.com>
2024-09-13 13:07:10 +0000

    [WAL-7245] Set project start date if proposal has been approved.

--- src/waldur_mastermind/proposal/apps.py
+++ src/waldur_mastermind/proposal/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from django.db.models import signals
 
 
 class ProposalConfig(AppConfig):
     name = "waldur_mastermind.proposal"
     verbose_name = "Proposal"
+
+    def ready(self):
+        from waldur_mastermind.proposal import models
+
+        from . import handlers
+
+        signals.post_save.connect(
+            handlers.set_project_start_date,
+            sender=models.Proposal,
+            dispatch_uid="waldur_mastermind.proposal.handlers.set_project_start_date",
+        )
