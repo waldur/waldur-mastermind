537dd69f40e5f2063dae96f9a626c93667ffd75f
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-10-16 00:20:53 +0300

    Fix migration to avoid pgsql extension

--- src/waldur_core/media/migrations/0002_data.py
+++ src/waldur_core/media/migrations/0002_data.py
@@ -1,17 +1,20 @@
+import uuid
+
 from django.db import migrations
+
+import waldur_core.core.fields
 
 SQL_QUERY = """
 DO $$
 BEGIN
     IF to_regclass('public.binary_database_files_file') IS NOT NULL THEN
-        INSERT INTO media_file (name, content, size, created, modified, uuid, mime_type, is_public)
+        INSERT INTO media_file (name, content, size, created, modified, mime_type, is_public)
         SELECT
             name,
             content,
             size,
             created_datetime AS created,
             created_datetime AS modified,
-            gen_random_uuid() AS uuid,
             'application/octet-stream' AS mime_type,
             false AS is_public
         FROM
@@ -22,11 +25,29 @@ END $$;
 """
 
 
+def gen_uuid(apps, schema_editor):
+    File = apps.get_model("media", "File")
+    for row in File.objects.all():
+        row.uuid = uuid.uuid4().hex
+        row.save(update_fields=["uuid"])
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("media", "0001_initial"),
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name="media_file",
+            name="uuid",
+            field=waldur_core.core.fields.UUIDField(null=True, blank=True),
+        ),
         migrations.RunSQL(SQL_QUERY),
+        migrations.RunPython(gen_uuid, elidable=True),
+        migrations.AlterField(
+            model_name="media_file",
+            name="uuid",
+            field=waldur_core.core.fields.UUIDField(),
+        ),
     ]
