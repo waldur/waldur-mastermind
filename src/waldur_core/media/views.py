c397b6af5f6aa79a4a775b4b041109d267e12911
Viktor Mirieiev <victor.mireyev@gmail.com>
2024-10-22 05:27:07 +0000

    Switch attachment download to protected lookups [WAL-7573]

--- src/waldur_core/media/views.py
+++ src/waldur_core/media/views.py
@@ -4,11 +4,28 @@ from django.http import Http404, HttpResponse
 from django.utils.http import content_disposition_header
 from rest_framework.views import APIView
 
+from waldur_core.core.models import User
+
 from . import models
 
 
+def check_file_permissions(file: models.File, user: User):
+    """
+    If file is part of attachment, user should have appropriate permissions.
+    """
+    from waldur_mastermind.support.models import Attachment
+
+    try:
+        attachment = Attachment.objects.get(file=file.name)
+    except Attachment.DoesNotExist:
+        return
+    if user.is_anonymous:
+        raise Http404
+    if not Attachment.objects.filter_for_user(user).filter(id=attachment.id).exists():
+        raise Http404
+
+
 class MediaView(APIView):
-    authentication_classes = ()
     permission_classes = ()
 
     def get(self, request, uuid):
@@ -16,6 +33,7 @@ class MediaView(APIView):
             file = models.File.objects.get(uuid=uuid)
         except models.File.DoesNotExist:
             raise Http404
+        check_file_permissions(file, request.user)
         filename = os.path.split(file.name)[-1]
         response = HttpResponse(file.content)
         response.headers["Content-Length"] = file.size
