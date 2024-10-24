9a74cba356054af5072564984a5e6b36ef983b7f
Ilja Livenson <ilja.livenson@gmail.com>
2024-10-24 20:58:58 +0300

    Remove leftover converter

--- src/waldur_mastermind/marketplace_slurm_remote/registrators.py
+++ src/waldur_mastermind/marketplace_slurm_remote/registrators.py
@@ -1,6 +1,3 @@
-import math
-from decimal import Decimal
-
 from waldur_mastermind.marketplace import registrators as marketplace_registrators
 
 from . import PLUGIN_NAME
@@ -8,13 +5,3 @@ from . import PLUGIN_NAME
 
 class RemoteSlurmRegistrator(marketplace_registrators.MarketplaceRegistrator):
     plugin_name = PLUGIN_NAME
-
-    @classmethod
-    def convert_quantity(cls, usage, component_type):
-        minutes_in_hour = 60
-        if component_type == "ram":
-            mb_in_gb = 1024
-            quantity = int(math.ceil(Decimal(1.0) * usage / mb_in_gb / minutes_in_hour))
-        else:
-            quantity = int(math.ceil(Decimal(1.0) * usage / minutes_in_hour))
-        return quantity
