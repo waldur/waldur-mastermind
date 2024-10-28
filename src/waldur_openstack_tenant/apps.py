3bd8ceebe9b82802051cd660b9fcfc69db2a36a2
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-10-28 18:03:08 +0200

    Drop leftover app

--- src/waldur_openstack_tenant/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenStackTenantConfig(AppConfig):
-    name = "waldur_openstack_tenant"
-    label = "openstack_tenant"
-    verbose_name = "OpenStackTenant"
-    service_name = "OpenStackTenant"
