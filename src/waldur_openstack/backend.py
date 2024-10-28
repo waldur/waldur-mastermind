b9253eb1657907bec83779672366be5bf062edc0
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2024-10-28 17:41:29 +0200

    Expose external IPs in fixed IPs list for OS router [WAL-7602]

--- src/waldur_openstack/backend.py
+++ src/waldur_openstack/backend.py
@@ -857,6 +857,10 @@ class OpenStackBackend(ServiceBackend):
                         ].startswith("fe80::"):
                             continue
                         fixed_ips.append(fixed_ip["ip_address"])
+                for fixed_ip in backend_router["external_gateway_info"][
+                    "external_fixed_ips"
+                ]:
+                    fixed_ips.append(fixed_ip["ip_address"])
             except neutron_exceptions.NeutronClientException as e:
                 raise OpenStackBackendError(e)
 
