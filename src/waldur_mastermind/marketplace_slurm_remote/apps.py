e4d40b86658c4de3fa76eb9dddc7192906f0b079
Sergei Zaiaev <sergei.zaiaev@ut.ee>
2024-11-21 10:21:30 +0000

    [WAL-7685] Analyse webhook extension to agent to support faster polling for changes in orders and membership

--- src/waldur_mastermind/marketplace_slurm_remote/apps.py
+++ src/waldur_mastermind/marketplace_slurm_remote/apps.py
@@ -9,6 +9,7 @@ class MarketplaceSlurmConfig(AppConfig):
 
     def ready(self):
         from waldur_mastermind.marketplace import handlers as marketplace_handlers
+        from waldur_mastermind.marketplace import models as marketplace_models
         from waldur_mastermind.marketplace.plugins import manager
         from waldur_mastermind.marketplace_slurm_remote import (
             PLUGIN_NAME,
@@ -43,4 +44,10 @@ class MarketplaceSlurmConfig(AppConfig):
             handlers.sync_component_user_usage_when_allocation_user_usage_is_submitted,
             sender=slurm_models.AllocationUserUsage,
             dispatch_uid="waldur_mastermind.marketplace_slurm_remote.sync_component_user_usage_when_allocation_user_usage_is_submitted",
+        )
+
+        signals.post_save.connect(
+            handlers.send_order_created_to_mqtt,
+            sender=marketplace_models.Order,
+            dispatch_uid="waldur_mastermind.marketplace_slurm_remote.send_order_created_to_mqtt",
         )
