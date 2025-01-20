edb2bda4657780d880cd6032d7b468fc304295d6
Maxim Prokopenko <maximprokopenko@gmail.com>
2025-01-20 10:13:14 +0100

    [WAL-8044] Fix conversion error.

--- src/waldur_mastermind/marketplace_slurm/registrators.py
+++ src/waldur_mastermind/marketplace_slurm/registrators.py
@@ -1,4 +1,5 @@
 import math
+from decimal import Decimal
 
 from waldur_mastermind.marketplace import registrators as marketplace_registrators
 
@@ -9,11 +10,12 @@ class SlurmRegistrator(marketplace_registrators.MarketplaceRegistrator):
     plugin_name = PLUGIN_NAME
 
     @classmethod
-    def convert_quantity(cls, usage, component_type):
+    def convert_quantity(cls, usage: int | float | Decimal, component_type: str) -> int:
         minutes_in_hour = 60
+        usage_float = float(usage)
         if component_type == "ram":
             mb_in_gb = 1024
-            quantity = int(math.ceil(1.0 * usage / mb_in_gb / minutes_in_hour))
+            quantity = int(math.ceil(usage_float / mb_in_gb / minutes_in_hour))
         else:
-            quantity = int(math.ceil(1.0 * usage / minutes_in_hour))
+            quantity = int(math.ceil(usage_float / minutes_in_hour))
         return quantity
