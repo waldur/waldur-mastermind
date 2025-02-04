4347365cef14e95980c1656a275f9a321eabe145
Sergei Zaiaev <serjzv@gmail.com>
2025-02-04 14:05:13 +0200

    Add generate_integration_user to the provider offering viewset

--- src/waldur_mastermind/marketplace/serializers.py
+++ src/waldur_mastermind/marketplace/serializers.py
@@ -1272,6 +1272,17 @@ class ProviderOfferingDetailsSerializer(
         }
 
 
+class GenerateIntegrationUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = core_models.User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+        )
+
+
 class PublicOfferingDetailsSerializer(ProviderOfferingDetailsSerializer):
     class Meta(ProviderOfferingDetailsSerializer.Meta):
         view_name = "marketplace-public-offering-detail"
