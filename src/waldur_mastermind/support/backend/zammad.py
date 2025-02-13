70849eb9b160118a929a07555f256bf026532b91
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-02-13 10:56:48 +0200

    Fix Zammad users pull [WAL-8074]

--- src/waldur_mastermind/support/backend/zammad.py
+++ src/waldur_mastermind/support/backend/zammad.py
@@ -321,8 +321,8 @@ class ZammadServiceBackend(SupportBackend):
         }
         local_ids = set(local_map.keys())
 
-        new_ids = local_ids - remote_ids
-        stale_ids = remote_ids - local_ids
+        new_ids = remote_ids - local_ids
+        stale_ids = local_ids - remote_ids
         common_ids = local_ids & remote_ids
 
         models.SupportUser.objects.filter(
