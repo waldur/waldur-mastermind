5c2a7cf6722007f14588fccb5f19e35e8140c35a
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-02-18 11:13:00 +0200

    Improve OpenAPI schema generator

--- src/waldur_core/core/openapi_inspector.py
+++ src/waldur_core/core/openapi_inspector.py
@@ -5,7 +5,10 @@ class WaldurOpenApiInspector(AutoSchema):
     def get_operation_id(self) -> str:
         path = self._tokenize_path()
 
-        use_short = getattr(self.view, "detail", False) and self.method == "POST"
+        use_short = (
+            getattr(self.view, "detail", False)
+            or getattr(self.view, "action", "") != "create"
+        ) and self.method == "POST"
 
         if not use_short:
             if self.method == "GET" and self._is_list_view():
