a6ec64385f2c325cc5d202b6a097a74a9376b919
Ilja Livenson <ilja.livenson@gmail.com>
2025-03-10 18:20:52 +0200

    Expose forwarded headers if available in gunicorn logs

--- docker/rootfs/etc/waldur/gunicorn.conf.py
+++ docker/rootfs/etc/waldur/gunicorn.conf.py
@@ -3,5 +3,8 @@ workers = 4
 forwarded_allow_ips = "*"
 proxy_allow_ips = "*"
 accesslog = "-"
+access_log_format = (
+    '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+)
 errorlog = "-"
 capture_output = True
