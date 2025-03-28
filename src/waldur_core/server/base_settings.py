5b3265275b057a9b009e2db6c73c7c14e768809d
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-03-28 17:56:25 +0200

    Adjust content types for PATCH, POST and PUT requests.

--- src/waldur_core/server/base_settings.py
+++ src/waldur_core/server/base_settings.py
@@ -1019,6 +1019,7 @@ SPECTACULAR_SETTINGS = {
         "waldur_core.core.schema_hooks.transform_paginated_arrays",
         "waldur_core.core.schema_hooks.make_fields_optional",
         "waldur_core.core.schema_hooks.remove_waldur_cookie_auth",
+        "waldur_core.core.schema_hooks.adjust_request_body_content_types",
     ],
     "DEFAULT_GENERATOR_CLASS": "waldur_core.core.openapi_generators.WaldurSchemaGenerator",
     "ENUM_GENERATE_CHOICE_DESCRIPTION": False,
