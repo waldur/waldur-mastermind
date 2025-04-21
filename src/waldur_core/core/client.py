0cd79c57fd5506ec03400e91d86947e415fa2478
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-04-21 11:07:45 +0300

    Fix Waldur client base URL

--- src/waldur_core/core/client.py
+++ src/waldur_core/core/client.py
@@ -3,7 +3,7 @@ from waldur_api_client.client import AuthenticatedClient
 
 def get_waldur_client(api_url, token):
     return AuthenticatedClient(
-        base_url=api_url.rstrip("/"),
+        base_url=api_url.rstrip("/api"),
         token=token,
         prefix="Token",
         raise_on_unexpected_status=True,
