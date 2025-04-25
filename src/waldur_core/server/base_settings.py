03f0dbbfc5bf49bffa21832309e93a70c0c2852a
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-04-25 15:17:52 +0000

    Fix Celery worker debug: avoid module name collision leading to partial import error.

--- src/waldur_core/server/base_settings.py
+++ src/waldur_core/server/base_settings.py
@@ -241,7 +241,7 @@ CELERY_TASK_QUEUES = {
     "background": {"exchange": "background"},
 }
 CELERY_TASK_DEFAULT_QUEUE = "tasks"
-CELERY_TASK_ROUTES = ("waldur_core.server.celery.PriorityRouter",)
+CELERY_TASK_ROUTES = ("waldur_core.server.celeryconf.PriorityRouter",)
 CELERY_TRACK_STARTED = True
 CELERY_SEND_EVENTS = True
 
