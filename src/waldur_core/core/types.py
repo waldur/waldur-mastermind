9f31349d7da18c9053b507c2e327554903ad3ef4
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-06-21 12:24:32 +0300

    Improve type

--- src/waldur_core/core/types.py
+++ src/waldur_core/core/types.py
@@ -1,6 +1,9 @@
+from waldur_core.permissions.models import Role
+
+
 class classproperty:
     def __init__(self, func):
         self.fget = func
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance, owner) -> Role:
         return self.fget(owner)
