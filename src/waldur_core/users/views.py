1a6d7b4f8569897b3a0530eee24cdadb5026a135
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-06-25 13:07:37 +0300

    Fix type check

--- src/waldur_core/users/views.py
+++ src/waldur_core/users/views.py
@@ -4,7 +4,7 @@ from constance import config
 from django.conf import settings
 from django.db import transaction
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema
 from rest_framework import permissions as rf_permissions
