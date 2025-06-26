b3256c2a3a6f5bbac2c28f3b97b109e5e5155078
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-06-26 13:31:15 +0300

    Expose customer for call permissions [WAL-8274]

--- src/waldur_mastermind/proposal/models.py
+++ src/waldur_mastermind/proposal/models.py
@@ -145,6 +145,10 @@ class Call(
     def reviewers(self):
         return get_users(self, RoleEnum.CALL_REVIEWER)
 
+    @property
+    def customer(self):
+        return self.manager.customer
+
 
 class RequestedOffering(
     SafeAttributesMixin,
