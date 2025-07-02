21adab79ddae9cdfbd3c14762922e5a6cac5da40
Viktor Mirieiev <victor.mireyev@gmail.com>
2025-07-02 15:14:56 +0000

    Revert "Drop deprecated annotation"
    This reverts commit d8d36faec9bc14e0a1f6959d09dbfdde8c29f295

--- src/waldur_core/core/nested_routers.py
+++ src/waldur_core/core/nested_routers.py
@@ -25,6 +25,8 @@ Example:
     urlpatterns = router.urls
 """
 
+from __future__ import annotations
+
 import re
 from typing import Any
 
@@ -44,7 +46,7 @@ class LookupMixin:
 class NestedMixin:
     def __init__(
         self,
-        parent_router: SimpleRouter | DefaultRouter | "NestedMixin",
+        parent_router: SimpleRouter | DefaultRouter | NestedMixin,
         parent_prefix: str,
         *args: Any,
         **kwargs: Any,
