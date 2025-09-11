a7030536187ab8dca6deb204ac8c4ac0c2b0e0d0
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-09-11 12:23:24 +0300

    Drop leftover prefix config for SDK

--- src/waldur_core/core/client.py
+++ src/waldur_core/core/client.py
@@ -39,8 +39,4 @@ def check_url(url):
 
 def get_waldur_client(api_url, token):
     check_url(api_url)
-    return AuthenticatedClient(
-        base_url=api_url.rstrip("/api"),
-        token=token,
-        prefix="Token",
-    )
+    return AuthenticatedClient(base_url=api_url.rstrip("/api"), token=token)
