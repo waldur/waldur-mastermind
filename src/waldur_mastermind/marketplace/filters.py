2ee3ce7ea214c66294ab09e9188a80116455e8f3
Viktor Mirieiev <viktor.mirieiev@ut.ee>
2025-09-25 21:28:19 +0300

    Fix offering_billable filter

--- src/waldur_mastermind/marketplace/filters.py
+++ src/waldur_mastermind/marketplace/filters.py
@@ -474,7 +474,7 @@ class ResourceFilter(
     )
 
     offering_type = django_filters.CharFilter(field_name="offering__type")
-    offering_billable = django_filters.UUIDFilter(field_name="offering__billable")
+    offering_billable = django_filters.BooleanFilter(field_name="offering__billable")
     project_uuid = django_filters.UUIDFilter(field_name="project__uuid")
     project_name = django_filters.CharFilter(field_name="project__name")
     customer_uuid = django_filters.UUIDFilter(field_name="project__customer__uuid")
