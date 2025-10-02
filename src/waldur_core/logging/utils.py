d407543e0ebe06de8da570ebf913c872eb7d55c8
Sergei Zaiaev <serjzv@gmail.com>
2025-10-02 10:30:28 +0300

    Improve STOMP connection cleanup

--- src/waldur_core/logging/utils.py
+++ src/waldur_core/logging/utils.py
@@ -131,6 +131,6 @@ def publish_stomp_messages(messages_to_send: list[dict[str, str]]) -> None:
                 "Failed to publish message to RabbitMQ STOMP queue: %s",
                 e,
             )
-
-    if connection and connection.is_connected():
-        connection.disconnect()
+        finally:
+            if connection and connection.is_connected():
+                connection.disconnect()
