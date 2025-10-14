eb2bb1b1b564c1de4397156dbc9c8b77b4344a07
Sergei Zaiaev <serjzv@gmail.com>
2025-10-14 15:45:45 +0300

    Move LoggingExtension celery tasks to site agent

--- src/waldur_core/logging/extension.py
+++ src/waldur_core/logging/extension.py
@@ -5,20 +5,3 @@ class LoggingExtension(WaldurExtension):
     @staticmethod
     def django_app():
         return "waldur_core.logging"
-
-    @staticmethod
-    def celery_tasks():
-        from datetime import timedelta
-
-        return {
-            "delete-stale-event-subscriptions": {
-                "task": "waldur_core.logging.delete_stale_event_subscriptions",
-                "schedule": timedelta(hours=24),
-                "args": (),
-            },
-            "delete-dangling-event-subscriptions": {
-                "task": "waldur_core.logging.delete_dangling_event_subscriptions",
-                "schedule": timedelta(hours=1),
-                "args": (),
-            },
-        }
--- src/waldur_mastermind/marketplace_site_agent/extension.py
+++ src/waldur_mastermind/marketplace_site_agent/extension.py
@@ -52,4 +52,14 @@ class MarketplaceSiteAgentExtension(WaldurExtension):
                 "schedule": timedelta(minutes=5),
                 "args": (),
             },
+            "delete-stale-event-subscriptions": {
+                "task": "waldur_core.logging.delete_stale_event_subscriptions",
+                "schedule": timedelta(hours=24),
+                "args": (),
+            },
+            "delete-dangling-event-subscriptions": {
+                "task": "waldur_core.logging.delete_dangling_event_subscriptions",
+                "schedule": timedelta(hours=1),
+                "args": (),
+            },
         }
