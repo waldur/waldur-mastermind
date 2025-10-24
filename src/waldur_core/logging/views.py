fea92f7c139e19609486d181d726a93366483411
Sergei Zaiaev <serjzv@gmail.com>
2025-10-24 12:26:22 +0300

    Improve source IP lookup for event subscriptions

--- src/waldur_core/logging/views.py
+++ src/waldur_core/logging/views.py
@@ -17,6 +17,7 @@ from rest_framework import (
 from waldur_core.core import filters as core_filters
 from waldur_core.core import models as core_models
 from waldur_core.core import permissions as core_permissions
+from waldur_core.core import utils as core_utils
 from waldur_core.core.managers import SummaryQuerySet
 from waldur_core.logging import backend, filters, models, serializers, utils
 from waldur_core.logging.event_logger import get_event_groups
@@ -211,7 +212,7 @@ class EventSubscriptionViewSet(
 
     def perform_create(self, serializer):
         user = self.request.user
-        source_ip = self.request.META.get("REMOTE_ADDR")
+        source_ip = core_utils.get_ip_address(self.request)
         serializer.save(user=user, source_ip=source_ip)
 
     def perform_destroy(self, instance):
