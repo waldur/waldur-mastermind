397e88c9911d2673ae2f70464ee58de329596812
Emil Ojagverdiyev <ojagverdiyev13@gmail.com>
2025-12-16 09:48:58 +0000

    [WAL-9477] Add filters for onboarding endpoints

--- src/waldur_core/onboarding/filters.py
+++ src/waldur_core/onboarding/filters.py
@@ -3,6 +3,39 @@ import django_filters
 from . import models
 
 
+class OnboardingVerificationFilter(django_filters.FilterSet):
+    user_uuid = django_filters.UUIDFilter(field_name="user__uuid", label="User UUID")
+    country = django_filters.CharFilter(field_name="country")
+    legal_name = django_filters.CharFilter(
+        field_name="legal_name", lookup_expr="icontains"
+    )
+    legal_person_identifier = django_filters.CharFilter(
+        field_name="legal_person_identifier", lookup_expr="icontains"
+    )
+    status = django_filters.CharFilter(field_name="status")
+
+    class Meta:
+        model = models.OnboardingVerification
+        fields = [
+            "user_uuid",
+            "country",
+            "legal_name",
+            "legal_person_identifier",
+            "status",
+        ]
+
+
+class OnboardingJustificationFilter(django_filters.FilterSet):
+    verification_uuid = django_filters.UUIDFilter(
+        field_name="verification__uuid", label="Verification UUID"
+    )
+    user_uuid = django_filters.UUIDFilter(field_name="user__uuid", label="User UUID")
+
+    class Meta:
+        model = models.OnboardingJustification
+        fields = ["verification_uuid", "user_uuid"]
+
+
 class OnboardingCountryChecklistConfigurationFilter(django_filters.FilterSet):
     country = django_filters.CharFilter(field_name="country")
     is_active = django_filters.BooleanFilter(field_name="is_active")
