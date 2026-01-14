2d688e2e297b01f2677a25b7667726b369d02758
Sergei Zaiaev <serjzv@gmail.com>
2026-01-14 16:21:17 +0200

    Fix RabbitMQ queue parameter

--- src/waldur_core/logging/utils.py
+++ src/waldur_core/logging/utils.py
@@ -146,7 +146,7 @@ def publish_stomp_messages(messages_to_send: list[dict[str, str]]) -> None:
                 "auto-delete": "false",  # Queue exists even without consumers
                 "content-type": "application/json",
                 "expiration": "86400000",  # Message TTL: 24 hours in milliseconds
-                "x-max-length": "10000",  # Max 10,000 messages per queue
+                "x-max-length": 10000,  # Max 10,000 messages per queue
             }
             logger.info(
                 "Sending STOMP message %s to %s", message_info["payload"], destination
