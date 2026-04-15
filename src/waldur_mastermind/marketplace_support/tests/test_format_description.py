27f98d17761914aad5d9ec34fc07bac303d7a3f0
Ilja Livenson <ilja.livenson@gmail.com>
2026-04-15 13:05:36 +0200

    Add start and end dates to support ticket description

---
+++ src/waldur_mastermind/marketplace_support/tests/test_format_description.py
@@ -0,0 +1,42 @@
+import datetime
+
+from django.test import TestCase
+
+from waldur_mastermind.marketplace.tests import factories as marketplace_factories
+from waldur_mastermind.marketplace_support import utils
+
+
+class FormatCreateDescriptionTest(TestCase):
+    def test_start_date_included_when_set(self):
+        order = marketplace_factories.OrderFactory(
+            start_date=datetime.date(2026, 6, 1),
+        )
+        description = utils.format_create_description(order)
+        self.assertIn("Start date: June 1, 2026", description)
+
+    def test_end_date_included_when_set(self):
+        order = marketplace_factories.OrderFactory()
+        order.resource.end_date = datetime.date(2026, 12, 31)
+        order.resource.save(update_fields=["end_date"])
+        description = utils.format_create_description(order)
+        self.assertIn("End date: Dec. 31, 2026", description)
+
+    def test_dates_included_when_both_set(self):
+        order = marketplace_factories.OrderFactory(
+            start_date=datetime.date(2026, 6, 1),
+        )
+        order.resource.end_date = datetime.date(2026, 12, 31)
+        order.resource.save(update_fields=["end_date"])
+        description = utils.format_create_description(order)
+        self.assertIn("Start date:", description)
+        self.assertIn("End date:", description)
+
+    def test_dates_not_included_when_not_set(self):
+        order = marketplace_factories.OrderFactory(
+            start_date=None,
+        )
+        order.resource.end_date = None
+        order.resource.save(update_fields=["end_date"])
+        description = utils.format_create_description(order)
+        self.assertNotIn("Start date:", description)
+        self.assertNotIn("End date:", description)
