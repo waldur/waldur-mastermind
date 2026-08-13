5e4940a24458cccaa08d75227d6106dfd3f99f15
Ilja Livenson <ilja.livenson@gmail.com>
2026-08-13 11:03:00 +0300

    Remove unused marketplace OpenStack category UUID settings

--- src/waldur_mastermind/marketplace_openstack/extension.py
+++ src/waldur_mastermind/marketplace_openstack/extension.py
@@ -1,7 +1,5 @@
 from datetime import timedelta
 
-from django.core.exceptions import ObjectDoesNotExist
-
 from waldur_core.core import WaldurExtension
 
 
@@ -14,30 +12,6 @@ class MarketplaceOpenStackExtension(WaldurExtension):
     @staticmethod
     def django_app():
         return "waldur_mastermind.marketplace_openstack"
-
-    @staticmethod
-    def get_dynamic_settings():
-        from waldur_mastermind.marketplace.models import Category
-
-        settings = {
-            item[0]: item[1]
-            for item in [
-                ("INSTANCE_CATEGORY_UUID", "vm"),
-                ("VOLUME_CATEGORY_UUID", "volume"),
-                ("TENANT_CATEGORY_UUID", "tenant"),
-            ]
-        }
-
-        for key, name in settings.items():
-            try:
-                predicate = {"default_%s_category" % name: True}
-                category_uuid = Category.objects.get(**predicate).uuid
-            except ObjectDoesNotExist:
-                settings[key] = None
-            else:
-                settings[key] = category_uuid
-
-        return settings
 
     @staticmethod
     def is_assembly():
