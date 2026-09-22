d486c540e4d95f307affb3964408a608b195fe4a
Ilja Livenson <ilja.livenson@gmail.com>
2026-09-22 14:51:39 +0300

    Renumber order placed_automatically migration to resolve leaf conflict

--- src/waldur_mastermind/marketplace/migrations/0297_order_placed_automatically.py
+++ src/waldur_mastermind/marketplace/migrations/0297_order_placed_automatically.py
@@ -3,7 +3,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("marketplace", "0295_offering_merge_progress"),
+        ("marketplace", "0296_clean_noop_price_logs"),
     ]
 
     operations = [
